adec00af682ab695034d789e95abb880c4788756
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 01:27:33 +0500

    Update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../core/theme/colors.dart';
+import '../../widgets/post_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,10 +12,27 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: const Text('Raonson'),
+        title: const Text(
+          "Raonson",
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.1,
+          ),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 14),
+            child: Icon(Icons.notifications_none),
+          )
+        ],
       ),
-      body: const Center(
-        child: Text('Home Feed', style: TextStyle(color: Colors.white)),
+      body: ListView(
+        padding: const EdgeInsets.all(14),
+        children: const [
+          PostCard(),
+          PostCard(),
+          PostCard(),
+        ],
       ),
     );
   }
