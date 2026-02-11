12b1e187a540a9da3d561f42a258489c8aab20c0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 01:22:07 +0500

    Create colors.dart

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RColors {
+  static const bg = Color(0xFF070B16);
+  static const card = Color(0xFF0E1424);
+  static const neon = Color(0xFF3DA5FF);
+  static const white = Colors.white;
+  static const muted = Colors.white54;
+}
