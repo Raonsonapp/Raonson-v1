49d9694d13a87f7315b3ddf8fc164dd8f0a9fe9d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 03:34:12 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import profileRoutes from "./routes/profile.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-// routes
 app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
-// health check
 app.get("/", (req, res) => {
   res.json({ status: "Raonson server is running" });
 });
