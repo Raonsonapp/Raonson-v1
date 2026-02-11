af4de533f29596c0e52ce6dde661ec1a9f79d1ea
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 03:14:19 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,13 @@
 import express from "express";
-import cors from "cors";
-import apiRoutes from "./routes/index.js";
 
 const app = express();
 
-app.use(cors());
+// middleware
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
   res.json({ status: "Raonson server is running" });
 });
-
-app.use("/api", apiRoutes);
 
 export default app;
