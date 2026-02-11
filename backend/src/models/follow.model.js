da849b07177093837bc7c28e4cc7281dd3ca5923
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 03:46:29 +0500

    Create follow.model.js

---
+++ backend/src/models/follow.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const followSchema = new mongoose.Schema(
+  {
+    followerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    followingId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// як корбар ду бор follow карда наметавонад
+followSchema.index(
+  { followerId: 1, followingId: 1 },
+  { unique: true }
+);
+
+export default mongoose.model("Follow", followSchema);
