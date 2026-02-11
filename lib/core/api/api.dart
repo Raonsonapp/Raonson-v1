20eba79d595fc38b7cdcb149d1a5c4267ea709b9
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 03:28:01 +0500

    Update api.dart

--- lib/core/api/api.dart
+++ lib/core/api/api.dart
@@ -1,29 +1,3 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-const String BASE_URL = "https://raonson-me.onrender.com/api";
-
-Future<http.Response> getRequest(String path, {String? token}) {
-  return http.get(
-    Uri.parse("$BASE_URL$path"),
-    headers: {
-      "Content-Type": "application/json",
-      if (token != null) "Authorization": "Bearer $token",
-    },
-  );
-}
-
-Future<http.Response> postRequest(
-  String path,
-  Map<String, dynamic> body, {
-  String? token,
-}) {
-  return http.post(
-    Uri.parse("$BASE_URL$path"),
-    headers: {
-      "Content-Type": "application/json",
-      if (token != null) "Authorization": "Bearer $token",
-    },
-    body: jsonEncode(body),
-  );
+class ApiConstants {
+  static const String baseUrl = "https://raonson-v1.onrender.com";
 }
