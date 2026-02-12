e2df9fc6151c2abe370eb3b2ac06f081b60418d5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:17:39 +0500

    Update auth_api.dart

--- lib/auth/auth_api.dart
+++ lib/auth/auth_api.dart
@@ -1,27 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../core/api.dart';
-import '../core/token_storage.dart';
-import 'auth_model.dart';
+import '../core/api/api.dart';
 
 class AuthApi {
-  static Future<AuthResult?> login(
-      String phone, String password) async {
-    final res = await http.post(
+  static Future<void> login(String email, String password) async {
+    await http.post(
       Uri.parse('${Api.baseUrl}/auth/login'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
-        'phone': phone,
+        'email': email,
         'password': password,
       }),
     );
-
-    if (res.statusCode != 200) return null;
-
-    final data = jsonDecode(res.body);
-    final result = AuthResult.fromJson(data);
-
-    await TokenStorage.save(result.token);
-    return result;
   }
 }
