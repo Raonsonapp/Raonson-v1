20b38f61d5cbf7680f6be3940e4da50804fec96d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 19:47:42 +0500

    Update feed_model.dart

--- lib/feed/feed_model.dart
+++ lib/feed/feed_model.dart
@@ -1,5 +1,6 @@
 class FeedPost {
   final String id;
+  final String userId;
   final String image;
   final String caption;
   final int likes;
@@ -7,6 +8,7 @@ class FeedPost {
 
   FeedPost({
     required this.id,
+    required this.userId,
     required this.image,
     required this.caption,
     required this.likes,
@@ -16,6 +18,7 @@ class FeedPost {
   factory FeedPost.fromJson(Map<String, dynamic> json) {
     return FeedPost(
       id: json['id'],
+      userId: json['userId'],
       image: json['image'],
       caption: json['caption'],
       likes: json['likes'],
