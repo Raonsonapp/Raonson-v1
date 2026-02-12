5358d9e51a1b723d4e2824223d99283fc99ceb1b
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:09:49 +0500

    Update chat_api.dart

--- lib/chat/chat_api.dart
+++ lib/chat/chat_api.dart
@@ -1,29 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../core/api/api.dart';
+import 'chat_model.dart';
 
 class ChatApi {
-  static Future<List<dynamic>> getChats() async {
-    final res = await http.get(
-      Uri.parse('${Api.baseUrl}/chats'),
-    );
-
-    return jsonDecode(res.body);
-  }
-
-  static Future<List<dynamic>> getMessages(String chatId) async {
+  static Future<List<Message>> getMessages(String chatId) async {
     final res = await http.get(
       Uri.parse('${Api.baseUrl}/chats/$chatId/messages'),
     );
 
-    return jsonDecode(res.body);
-  }
-
-  static Future<void> sendMessage(String chatId, String text) async {
-    await http.post(
-      Uri.parse('${Api.baseUrl}/chats/$chatId/messages'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'text': text}),
-    );
+    final List data = jsonDecode(res.body);
+    return data.map((e) => Message.fromJson(e)).toList();
   }
 }
