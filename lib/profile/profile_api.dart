47d084f289f11182b93659ea99871f87e791c52a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 19:58:59 +0500

    Update profile_api.dart

--- lib/profile/profile_api.dart
+++ lib/profile/profile_api.dart
@@ -1,27 +1,24 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../core/api.dart';
-import '../core/token_storage.dart';
+import '../core/api/api.dart';
 import 'profile_model.dart';
 
 class ProfileApi {
   static Future<Profile> getProfile(String userId) async {
-    final token = await TokenStorage.read();
-
     final res = await http.get(
       Uri.parse('${Api.baseUrl}/profile/$userId'),
-      headers: {'Authorization': 'Bearer $token'},
     );
+
+    if (res.statusCode != 200) {
+      throw Exception('Failed to load profile');
+    }
 
     return Profile.fromJson(jsonDecode(res.body));
   }
 
   static Future<void> toggleFollow(String userId) async {
-    final token = await TokenStorage.read();
-
     await http.post(
       Uri.parse('${Api.baseUrl}/follow/$userId'),
-      headers: {'Authorization': 'Bearer $token'},
     );
   }
 }
