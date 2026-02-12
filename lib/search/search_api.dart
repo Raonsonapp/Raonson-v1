ba4350f8e0ac91af854164f5c16995cd6c30a618
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:01:47 +0500

    Update search_api.dart

--- lib/search/search_api.dart
+++ lib/search/search_api.dart
@@ -1,26 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../core/api.dart';
-import '../core/token_storage.dart';
-import 'search_model.dart';
+import '../core/api/api.dart';
 
 class SearchApi {
-  static Future<Map<String, dynamic>> search(String q) async {
-    final token = await TokenStorage.read();
-
+  static Future<List<dynamic>> search(String q) async {
     final res = await http.get(
       Uri.parse('${Api.baseUrl}/search?q=$q'),
-      headers: {'Authorization': 'Bearer $token'},
     );
 
-    final data = jsonDecode(res.body);
-    return {
-      'users': (data['users'] as List)
-          .map((e) => SearchUser.fromJson(e))
-          .toList(),
-      'posts': (data['posts'] as List)
-          .map((e) => SearchPost.fromJson(e))
-          .toList(),
-    };
+    return jsonDecode(res.body);
   }
 }
