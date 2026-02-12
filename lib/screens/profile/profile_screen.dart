b383b1ec7f76a1425dfb1e92ac07320f7e1da267
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 21:47:35 +0500

    Create profile_screen.dart

---
+++ lib/screens/profile/profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final String userId;
+
+  const ProfileScreen({super.key, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Profile (MVP)',
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
