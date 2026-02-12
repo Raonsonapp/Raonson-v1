b9e88306a4113b643ae149e9e2c8bd4d165bbb5f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 21:25:00 +0500

    git commit -m "Clean MVP: remove broken services, utils, stories, search logic" git push

--- lib/search/search_screen.dart
+++ lib/search/search_screen.dart
@@ -1,86 +1,14 @@
 import 'package:flutter/material.dart';
-import 'search_api.dart';
-import 'search_model.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  final ctrl = TextEditingController();
-  users = (res['users'] as List)
-    .map((e) => SearchUser.fromJson(e))
-    .toList();
-
-  posts = (res['posts'] as List)
-    .map((e) => SearchPost.fromJson(e))
-    .toList();
-  bool loading = false;
-
-  Future<void> runSearch(String q) async {
-    if (q.isEmpty) return;
-    setState(() => loading = true);
-    final res = await SearchApi.search(q);
-    setState(() {
-      users = (res['users'] as List)
-    .map((e) => SearchUser.fromJson(e))
-    .toList();
-
-      posts = (res['posts'] as List)
-    .map((e) => SearchPost.fromJson(e))
-    .toList();
-      loading = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: ctrl,
-          decoration: const InputDecoration(
-            hintText: 'Search',
-            border: InputBorder.none,
-          ),
-          onSubmitted: runSearch,
-        ),
+    return const Scaffold(
+      body: Center(
+        child: Text('Search (MVP)'),
       ),
-      body: loading
-          ? const Center(child: CircularProgressIndicator())
-          : ListView(
-              children: [
-                if (users.isNotEmpty)
-                  const Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text('Users',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  ),
-                ...users.map((u) => ListTile(
-                      leading: const CircleAvatar(
-                        child: Icon(Icons.person),
-                      ),
-                      title: Text(u.username),
-                      onTap: () {
-                        // open ProfileScreen(userId: u.id)
-                      },
-                    )),
-
-                if (posts.isNotEmpty)
-                  const Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Text('Posts',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  ),
-                ...posts.map((p) => ListTile(
-                      leading: const Icon(Icons.image),
-                      title: Text(p.caption),
-                    )),
-              ],
-            ),
     );
   }
 }
