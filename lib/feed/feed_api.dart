8fc0c5aea0111d813d42323bc538095f9a583897
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:00:03 +0500

    Update feed_api.dart

--- lib/feed/feed_api.dart
+++ lib/feed/feed_api.dart
@@ -1,26 +1,29 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'feed_model.dart';
+import '../core/api/api.dart';
 
 class FeedApi {
-  static const String baseUrl = 'https://raonson-v1.onrender.com';
-
-  static Future<List<FeedPost>> getFeed() async {
-    final res = await http.get(Uri.parse('$baseUrl/posts'));
+  static Future<List<dynamic>> getPosts() async {
+    final res = await http.get(
+      Uri.parse('${Api.baseUrl}/posts'),
+    );
 
     if (res.statusCode != 200) {
-      throw Exception('Failed to load feed');
+      throw Exception('Failed to load posts');
     }
 
-    final List data = jsonDecode(res.body);
-    return data.map((e) => FeedPost.fromJson(e)).toList();
+    return jsonDecode(res.body);
   }
 
-  static Future<void> like(String postId) async {
-    await http.post(Uri.parse('$baseUrl/likes/$postId'));
+  static Future<void> likePost(String postId) async {
+    await http.post(
+      Uri.parse('${Api.baseUrl}/likes/$postId'),
+    );
   }
 
-  static Future<void> save(String postId) async {
-    await http.post(Uri.parse('$baseUrl/save/$postId'));
+  static Future<void> savePost(String postId) async {
+    await http.post(
+      Uri.parse('${Api.baseUrl}/save/$postId'),
+    );
   }
 }
