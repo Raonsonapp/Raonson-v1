cabbffd8bae37642128e9a979435e5fdcc5b7fb5
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 19:48:02 +0500

    Update feed_api.dart

--- lib/feed/feed_api.dart
+++ lib/feed/feed_api.dart
@@ -3,25 +3,24 @@ import 'package:http/http.dart' as http;
 import 'feed_model.dart';
 
 class FeedApi {
-  static const String baseUrl =
-      'https://raonson-v1.onrender.com';
+  static const String baseUrl = 'https://raonson-v1.onrender.com';
 
-  static Future<List<FeedPost>> getPosts() async {
+  static Future<List<FeedPost>> getFeed() async {
     final res = await http.get(Uri.parse('$baseUrl/posts'));
 
     if (res.statusCode != 200) {
-      throw Exception('Failed to load posts');
+      throw Exception('Failed to load feed');
     }
 
     final List data = jsonDecode(res.body);
     return data.map((e) => FeedPost.fromJson(e)).toList();
   }
 
-  static Future<void> likePost(String postId) async {
+  static Future<void> like(String postId) async {
     await http.post(Uri.parse('$baseUrl/likes/$postId'));
   }
 
-  static Future<void> savePost(String postId) async {
+  static Future<void> save(String postId) async {
     await http.post(Uri.parse('$baseUrl/save/$postId'));
   }
 }
