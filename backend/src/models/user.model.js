12deea9391420a0de576c81fac2d10e4ebbfed26
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 22:56:33 +0500

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,8 +4,14 @@ const userSchema = new mongoose.Schema(
   {
     phone: { type: String, unique: true, required: true },
     email: { type: String },
-    password: { type: String },
+
     verified: { type: Boolean, default: false },
+
+    otpCode: { type: String },
+    otpExpire: { type: Date },
+
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
