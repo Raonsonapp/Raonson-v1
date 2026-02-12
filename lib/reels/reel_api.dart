5333af566454e432f63c38c69b00cece99e02e16
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:16:21 +0500

    Update reel_api.dart

--- lib/reels/reel_api.dart
+++ lib/reels/reel_api.dart
@@ -1,22 +1,14 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../core/api/api.dart';
 
 class ReelApi {
-  static Future<List<dynamic>> getReels() async {
-    final res = await http.get(
-      Uri.parse('${Api.baseUrl}/reels'),
-    );
-    return jsonDecode(res.body);
-  }
-
-  static Future<void> like(String id) async {
+  static Future<void> toggleLike(String id) async {
     await http.post(
       Uri.parse('${Api.baseUrl}/reels/like/$id'),
     );
   }
 
-  static Future<void> save(String id) async {
+  static Future<void> toggleSave(String id) async {
     await http.post(
       Uri.parse('${Api.baseUrl}/reels/save/$id'),
     );
