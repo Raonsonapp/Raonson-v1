a39a2fddf17198969c84f46ff51ff4b3b71555fb
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:10:29 +0500

    Update reel_api.dart

--- lib/reels/reel_api.dart
+++ lib/reels/reel_api.dart
@@ -1,41 +1,30 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../core/api.dart';
-import '../core/token_storage.dart';
-import 'reel_model.dart';
+import '../core/api/api.dart';
 
 class ReelApi {
-  static Future<List<Reel>> getReels() async {
-    final token = await TokenStorage.read();
+  static Future<List<dynamic>> getReels() async {
     final res = await http.get(
       Uri.parse('${Api.baseUrl}/reels'),
-      headers: {'Authorization': 'Bearer $token'},
     );
-    final data = jsonDecode(res.body) as List;
-    return data.map((e) => Reel.fromJson(e)).toList();
+    return jsonDecode(res.body);
   }
 
-  static Future<void> toggleLike(String id) async {
-    final token = await TokenStorage.read();
+  static Future<void> like(String id) async {
     await http.post(
       Uri.parse('${Api.baseUrl}/reels/like/$id'),
-      headers: {'Authorization': 'Bearer $token'},
     );
   }
 
-  static Future<void> toggleSave(String id) async {
-    final token = await TokenStorage.read();
+  static Future<void> save(String id) async {
     await http.post(
       Uri.parse('${Api.baseUrl}/reels/save/$id'),
-      headers: {'Authorization': 'Bearer $token'},
     );
   }
 
-  static Future<void> addView(String id) async {
-    final token = await TokenStorage.read();
+  static Future<void> view(String id) async {
     await http.post(
       Uri.parse('${Api.baseUrl}/reels/view/$id'),
-      headers: {'Authorization': 'Bearer $token'},
     );
   }
 }
