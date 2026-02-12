4f380aa657c3687525fb158880f7457ad6365f2e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 20:09:19 +0500

    Update chat_model.dart

--- lib/chat/chat_model.dart
+++ lib/chat/chat_model.dart
@@ -1,41 +1,18 @@
-class Chat {
-  final String id;
-  final String userId;
-  final String username;
-  final String lastMessage;
-
-  Chat({
-    required this.id,
-    required this.userId,
-    required this.username,
-    required this.lastMessage,
-  });
-
-  factory Chat.fromJson(Map<String, dynamic> json) {
-    return Chat(
-      id: json['_id'],
-      userId: json['userId'],
-      username: json['username'] ?? 'user',
-      lastMessage: json['lastMessage'] ?? '',
-    );
-  }
-}
-
 class Message {
   final String id;
-  final String senderId;
+  final String sender;
   final String text;
 
   Message({
     required this.id,
-    required this.senderId,
+    required this.sender,
     required this.text,
   });
 
   factory Message.fromJson(Map<String, dynamic> json) {
     return Message(
-      id: json['_id'],
-      senderId: json['senderId'],
+      id: json['id'],
+      sender: json['sender'],
       text: json['text'],
     );
   }
