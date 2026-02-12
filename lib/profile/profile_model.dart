8ab5d805ee2688eb5dca70dbc5884b4cfb6767e2
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-12 19:59:29 +0500

    Update profile_model.dart

--- lib/profile/profile_model.dart
+++ lib/profile/profile_model.dart
@@ -1,7 +1,6 @@
 class Profile {
   final String id;
   final String username;
-  final String bio;
   final int posts;
   final int followers;
   final int following;
@@ -10,7 +9,6 @@ class Profile {
   Profile({
     required this.id,
     required this.username,
-    required this.bio,
     required this.posts,
     required this.followers,
     required this.following,
@@ -19,13 +17,12 @@ class Profile {
 
   factory Profile.fromJson(Map<String, dynamic> json) {
     return Profile(
-      id: json['_id'] ?? '',
-      username: json['username'] ?? 'user',
-      bio: json['bio'] ?? '',
-      posts: json['posts'] ?? 0,
-      followers: json['followers'] ?? 0,
-      following: json['following'] ?? 0,
-      isFollowing: json['isFollowing'] ?? false,
+      id: json['id'],
+      username: json['username'],
+      posts: json['posts'],
+      followers: json['followers'],
+      following: json['following'],
+      isFollowing: json['isFollowing'],
     );
   }
 }
