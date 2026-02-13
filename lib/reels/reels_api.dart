04d3905e6a862055b2c528adf5ba33812902dcfc
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 21:41:05 +0500

    Update reels_api.dart

--- lib/reels/reels_api.dart
+++ lib/reels/reels_api.dart
@@ -2,20 +2,26 @@ import '../models/reel_model.dart';
 
 class ReelsApi {
   static Future<List<Reel>> getReels() async {
-    await Future.delayed(const Duration(milliseconds: 500));
+    await Future.delayed(const Duration(milliseconds: 400));
 
     return [
       Reel(
         id: '1',
         username: 'olivia_martin',
         caption: 'Sunset vibes 🌅',
-        videoUrl: 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+        videoUrl:
+            'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+        likes: 0,
+        liked: false,
       ),
       Reel(
         id: '2',
         username: 'alex_dev',
         caption: 'City night ✨',
-        videoUrl: 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+        videoUrl:
+            'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+        likes: 0,
+        liked: false,
       ),
     ];
   }
