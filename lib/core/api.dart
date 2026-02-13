9a2f3a338c62566b635d3aaf81b6fa38ab90f414
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 18:33:21 +0500

    Update api.dart

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -1,19 +1,30 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'constants.dart';
 import 'session.dart';
+import 'constants.dart';
 
 class Api {
+  static Future<http.Response> get(String path) async {
+    final token = Session.token;
+    return http.get(
+      Uri.parse('${Constants.baseUrl}$path'),
+      headers: {
+        'Content-Type': 'application/json',
+        if (token != null) 'Authorization': 'Bearer $token',
+      },
+    );
+  }
+
   static Future<http.Response> post(
     String path,
     Map<String, dynamic> body,
-  ) {
+  ) async {
+    final token = Session.token;
     return http.post(
-      Uri.parse('${AppConstants.baseUrl}$path'),
+      Uri.parse('${Constants.baseUrl}$path'),
       headers: {
         'Content-Type': 'application/json',
-        if (Session.token != null)
-          'Authorization': 'Bearer ${Session.token}',
+        if (token != null) 'Authorization': 'Bearer $token',
       },
       body: jsonEncode(body),
     );
