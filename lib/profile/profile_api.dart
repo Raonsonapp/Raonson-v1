ef035ce85c0a976839647b360c4f840c9572be86
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 19:21:22 +0500

    Update profile_api.dart

--- lib/profile/profile_api.dart
+++ lib/profile/profile_api.dart
@@ -1,6 +1,14 @@
+import 'dart:convert';
 import '../core/api.dart';
 
 class ProfileApi {
+  /// GET /profile/:userId
+  static Future<Map<String, dynamic>> getProfile(String userId) async {
+    final res = await Api.get('/profile/$userId');
+    return jsonDecode(res.body);
+  }
+
+  /// POST /follow/:userId
   static Future<void> follow(String userId) async {
     await Api.post('/follow/$userId');
   }
