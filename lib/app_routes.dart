15362769f9db9f0921c180be2302ed2587bf3cdd
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 18:14:28 +0500

    Update app_routes.dart

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -6,11 +6,18 @@ import 'bottom_nav/bottom_nav.dart';
 class AppRoutes {
   static const login = '/login';
   static const register = '/register';
-  static const app = '/app';
+  static const home = '/home';
 
-  static final routes = <String, WidgetBuilder>{
-    login: (_) => const LoginScreen(),
-    register: (_) => const RegisterScreen(),
-    app: (_) => const BottomNav(),
-  };
+  static Route<dynamic> generate(RouteSettings settings) {
+    switch (settings.name) {
+      case login:
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      case register:
+        return MaterialPageRoute(builder: (_) => RegisterScreen());
+      case home:
+        return MaterialPageRoute(builder: (_) => BottomNav());
+      default:
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+    }
+  }
 }
