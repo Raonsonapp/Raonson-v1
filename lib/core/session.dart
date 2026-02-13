d78343e2e5b889a76d8c4676457fe21117af9462
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 18:33:41 +0500

    Update session.dart

--- lib/core/session.dart
+++ lib/core/session.dart
@@ -1,4 +1,13 @@
 class Session {
   static String? token;
+
   static bool get isLoggedIn => token != null;
+
+  static void setToken(String t) {
+    token = t;
+  }
+
+  static void clear() {
+    token = null;
+  }
 }
