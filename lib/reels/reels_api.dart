541c834bd482468697669b12f450d201708b23ce
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 18:37:35 +0500

    Create reels_api.dart

---
+++ lib/reels/reels_api.dart
@@ -0,0 +1,16 @@
+import '../core/api.dart';
+import 'dart:convert';
+
+class ReelsApi {
+  static Future<List<dynamic>> getReels() async {
+    final res = await Api.get('/reels');
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    }
+    throw Exception('Reels load error');
+  }
+
+  static Future<void> likeReel(String reelId) async {
+    await Api.post('/reels/$reelId/like', {});
+  }
+}
