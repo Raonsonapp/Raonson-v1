6d758a51702a8b3eb012d66fda06ec71dfacda30
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 17:25:11 +0500

    Create bottom_nav.dart

---
+++ lib/bottom_nav/bottom_nav.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../home/home_screen.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int index = 0;
+
+  final screens = const [
+    HomeScreen(),
+    Center(child: Text('Reels')),
+    Center(child: Text('Create')),
+    Center(child: Text('Profile')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) => setState(() => index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.play_arrow), label: 'Reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Create'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
