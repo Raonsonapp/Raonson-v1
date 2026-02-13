6889945f92dd32e6666237a8d9d9d2004df865c4
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-13 20:49:38 +0500

    Update reel_model.dart

--- lib/models/reel_model.dart
+++ lib/models/reel_model.dart
@@ -1,1 +1,15 @@
+class Reel {
+  final String id;
+  final String user;
+  final String caption;
+  int likes;
+  bool liked;
 
+  Reel({
+    required this.id,
+    required this.user,
+    required this.caption,
+    this.likes = 0,
+    this.liked = false,
+  });
+}
