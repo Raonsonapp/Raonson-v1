dfd1cc442512d436183a44e70d0b180c0eff963c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 11:45:25 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const RaonsonApp());
 }
