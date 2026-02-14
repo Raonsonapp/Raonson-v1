ae664d2b00c7297401a9fa03bfef0251a1d10df0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 00:12:37 +0500

    Update comments_api.dart

--- lib/comments/comments_api.dart
+++ lib/comments/comments_api.dart
@@ -1,30 +1,40 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../core/constants.dart';
+import '../core/api.dart';
 import 'comment_model.dart';
 
-class CommentsApi {
-  static Future<List<Comment>> getComments(String reelId) async {
-    final res = await http.get(
-      Uri.parse('${Constants.baseUrl}/comments/$reelId'),
-    );
+class CommentApi {
+  /// GET COMMENTS FOR REEL
+  static Future<List<Comment>> fetchComments(String reelId) async {
+    final res = await Api.get('/comments/$reelId');
+
+    if (res.statusCode != 200) {
+      throw Exception('Failed to load comments');
+    }
 
     final List data = jsonDecode(res.body);
     return data.map((e) => Comment.fromJson(e)).toList();
   }
 
-  static Future<void> addComment(
-    String reelId,
-    String text,
-    String user,
-  ) async {
-    await http.post(
-      Uri.parse('${Constants.baseUrl}/comments/$reelId'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'text': text,
-        'user': user,
-      }),
+  /// ADD COMMENT
+  static Future<Comment> addComment({
+    required String reelId,
+    required String text,
+    required String token, // auth token
+  }) async {
+    final res = await http.post(
+      Uri.parse('${Api.baseUrl}/comments/$reelId'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode({'text': text}),
     );
+
+    if (res.statusCode != 200 && res.statusCode != 201) {
+      throw Exception('Failed to add comment');
+    }
+
+    return Comment.fromJson(jsonDecode(res.body));
   }
 }
