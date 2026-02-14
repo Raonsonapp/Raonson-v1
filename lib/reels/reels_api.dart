ba7896571d7a0452877e360e679a2082dfe0885c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 22:38:00 +0500

    Update reels_api.dart

--- lib/reels/reels_api.dart
+++ lib/reels/reels_api.dart
@@ -3,33 +3,34 @@ import '../core/api.dart';
 import '../models/reel_model.dart';
 
 class ReelsApi {
+  // 🔹 GET REELS FEED
+  static Future<List<Reel>> fetchReels() async {
+    final res = await Api.get('/reels');
+
+    final List data = jsonDecode(res.body);
+
+    return data.map<Reel>((e) {
+      return Reel(
+        id: e['id'].toString(),
+        username: e['username'] ?? 'user',
+        caption: e['caption'] ?? '',
+        videoUrl: e['videoUrl'],
+        likes: e['likes'] ?? 0,
+        views: e['views'] ?? 0,
+        liked: false,
+      );
+    }).toList();
+  }
+
+  // 🔹 ADD VIEW
   static Future<void> view(String reelId) async {
     await Api.post('/reels/$reelId/view');
   }
 
-  static Future<void> like(String reelId) async {
-    await Api.post('/reels/$reelId/like');
-  }
-}
-class ReelsApi {
-  static Future<List<Reel>> fetchReels() async {
-    final res = await Api.get("/reels");
-    final List data = jsonDecode(res.body);
-
-    return data.map<Reel>((e) => Reel(
-      id: e['id'],
-      videoUrl: e['videoUrl'],
-      caption: e['caption'] ?? '',
-      likes: e['likes'] ?? 0,
-      views: e['views'] ?? 0,
-    )).toList();
-  }
-
-  static Future<void> addView(String id) async {
-    await Api.post("/reels/$id/view");
-  }
-
-  static Future<void> like(String id) async {
-    await Api.post("/reels/$id/like");
+  // 🔹 LIKE
+  static Future<int> like(String reelId) async {
+    final res = await Api.post('/reels/$reelId/like');
+    final data = jsonDecode(res.body);
+    return data['likes'];
   }
 }
