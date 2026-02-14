9b88724ffe1b981bae55ed143a8a2df199e090fe
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 15:07:44 +0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -25,29 +25,56 @@ class _MainNavigationState extends State<MainNavigation> {
   int index = 1;
 
   final pages = const [
-    Center(child: Text('HOME')),
-    ReelsScreen(), // 👈 зинда
-    Center(child: Text('SEARCH')),
-    Center(child: Text('PROFILE')),
+    Center(child: Text('')),
+    ReelsScreen(),
+    Center(child: Text('')),
+    Center(child: Text('')),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: index,
-        onTap: (i) => setState(() => index = i),
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.play_circle_outline), label: 'Reels'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Profile'),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blueAccent.withOpacity(0.6),
+              blurRadius: 20,
+              spreadRadius: 1,
+              offset: const Offset(0, -4),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: index,
+          onTap: (i) => setState(() => index = i),
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white70,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.play_circle_outline),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: '',
+            ),
+          ],
+        ),
       ),
     );
   }
