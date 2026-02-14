f2ef5572235e36fab62b730fa42bc75cde7945a9
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 21:37:58 +0500

    Update reel_model.dart

--- lib/models/reel_model.dart
+++ lib/models/reel_model.dart
@@ -2,31 +2,24 @@ class Reel {
   final String id;
   final String videoUrl;
   final String caption;
-  final String username;
-
   int likes;
-  int comments;
-  bool liked;
+  int views;
 
   Reel({
     required this.id,
     required this.videoUrl,
     required this.caption,
-    required this.username,
-    this.likes = 0,
-    this.comments = 0,
-    this.liked = false,
+    required this.likes,
+    required this.views,
   });
 
   factory Reel.fromJson(Map<String, dynamic> json) {
     return Reel(
-      id: json['_id'],
+      id: json['id'],
       videoUrl: json['videoUrl'],
-      caption: json['caption'] ?? '',
-      username: json['username'] ?? 'user',
-      likes: json['likesCount'] ?? 0,
-      comments: json['commentsCount'] ?? 0,
-      liked: false, // сервер баъд медиҳад
+      caption: json['caption'],
+      likes: json['likes'],
+      views: json['views'],
     );
   }
 }
