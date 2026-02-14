491ff4d7fbd5fefecd3b654fa3ee11d9adf924d1
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 22:06:55 +0500

    Update reels_api.dart

--- lib/reels/reels_api.dart
+++ lib/reels/reels_api.dart
@@ -1,20 +1,25 @@
+import 'dart:convert';
 import '../core/api.dart';
 import '../models/reel_model.dart';
 
 class ReelsApi {
   static Future<List<Reel>> fetchReels() async {
-    final data = await Api.get('/reels');
+    final res = await Api.get("/reels");
+    final data = jsonDecode(res.body);
 
-    return (data as List)
-        .map((e) => Reel.fromJson(e))
-        .toList();
-  }
-
-  static Future<void> like(String id) async {
-    await Api.post('/reels/$id/like');
+    return data.map<Reel>((e) => Reel(
+      id: e['id'],
+      videoUrl: e['videoUrl'],
+      caption: e['caption'],
+      likes: e['likes'],
+    )).toList();
   }
 
   static Future<void> view(String id) async {
-    await Api.post('/reels/$id/view');
+    await Api.post("/reels/$id/view");
+  }
+
+  static Future<void> like(String id) async {
+    await Api.post("/reels/$id/like");
   }
 }
