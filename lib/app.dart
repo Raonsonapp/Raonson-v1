1e20491dd631efc1f68fde3d75b0e8fe6990d930
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 12:51:34 +0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const RaonsonApp());
+}
+
 class RaonsonApp extends StatelessWidget {
   const RaonsonApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeHome(),
+      theme: ThemeData.dark(),
+      home: const MainNavigation(),
     );
   }
 }
 
-class SafeHome extends StatelessWidget {
-  const SafeHome({super.key});
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int index = 0;
+
+  final pages = const [
+    Center(child: Text('HOME', style: TextStyle(fontSize: 24))),
+    Center(child: Text('REELS', style: TextStyle(fontSize: 24))),
+    Center(child: Text('SEARCH', style: TextStyle(fontSize: 24))),
+    Center(child: Text('PROFILE', style: TextStyle(fontSize: 24))),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'RAONSON OK',
-          style: TextStyle(color: Colors.white, fontSize: 24),
-        ),
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) => setState(() => index = i),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_library), label: 'Reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
       ),
     );
   }
