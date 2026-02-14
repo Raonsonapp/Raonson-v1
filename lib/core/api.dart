29fd23898ba658064055ab270bbaccb6a46c8ec6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 21:37:31 +0500

    Update api.dart

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -6,23 +6,13 @@ class Api {
   static Future<dynamic> get(String path) async {
     final res = await http.get(
       Uri.parse('${Constants.baseUrl}$path'),
-      headers: {'Content-Type': 'application/json'},
     );
     return jsonDecode(res.body);
   }
 
-  static Future<dynamic> post(
-    String path, {
-    Map<String, dynamic>? body,
-    String? token,
-  }) async {
+  static Future<dynamic> post(String path) async {
     final res = await http.post(
       Uri.parse('${Constants.baseUrl}$path'),
-      headers: {
-        'Content-Type': 'application/json',
-        if (token != null) 'Authorization': 'Bearer $token',
-      },
-      body: jsonEncode(body ?? {}),
     );
     return jsonDecode(res.body);
   }
