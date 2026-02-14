cfb9c7b4746e29a3f0015541495af7dcab2d2251
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 23:24:56 +0500

    Update comment.routes.js

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -1,15 +1,9 @@
 import express from "express";
 import { addComment, getComments } from "../controllers/comment.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-// COMMENTS FOR REELS
 router.get("/reels/:targetId", getComments);
-router.post("/reels/:targetId", authMiddleware, addComment);
-
-// COMMENTS FOR POSTS
-router.get("/posts/:targetId", getComments);
-router.post("/posts/:targetId", authMiddleware, addComment);
+router.post("/reels/:targetId", addComment);
 
 export default router;
