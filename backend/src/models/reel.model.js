5c8f24afcf18690628916ba001fe5782dd566b15
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 20:22:30 +0500

    Update reel.model.js

--- backend/src/models/reel.model.js
+++ backend/src/models/reel.model.js
@@ -2,11 +2,22 @@ import mongoose from "mongoose";
 
 const reelSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
     videoUrl: { type: String, required: true },
     caption: { type: String },
+
+    // 🔴 ЗИНДА
     likesCount: { type: Number, default: 0 },
+    commentsCount: { type: Number, default: 0 },
     viewsCount: { type: Number, default: 0 },
+
+    // 👇 кӣ лайк кард
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
