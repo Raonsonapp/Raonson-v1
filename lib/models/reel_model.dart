34b09ddfe8828e240031b1c6a5bbdf474f70b101
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 22:38:56 +0500

    Update reel_model.dart

--- lib/models/reel_model.dart
+++ lib/models/reel_model.dart
@@ -1,16 +1,17 @@
 class Reel {
   final String id;
-  final String videoUrl;
+  final String username;
   final String caption;
-
+  final String videoUrl;
   int likes;
   int views;
   bool liked;
 
   Reel({
     required this.id,
-    required this.videoUrl,
+    required this.username,
     required this.caption,
+    required this.videoUrl,
     required this.likes,
     required this.views,
     this.liked = false,
