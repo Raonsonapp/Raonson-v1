ff54d1b1f08610ca8adb30b7858b7a62d64274c1
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-14 21:25:27 +0500

    Update reel.routes.js

--- backend/src/routes/reel.routes.js
+++ backend/src/routes/reel.routes.js
@@ -1,19 +1,14 @@
 import express from "express";
 import {
-  createReel,
   getReels,
   addView,
   toggleLike,
 } from "../controllers/reel.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, createReel);
 router.get("/", getReels);
-router.post("/view/:reelId", addView);
-
-// ❤️ LIKE
-router.post("/like/:reelId", authMiddleware, toggleLike);
+router.post("/:id/view", addView);
+router.post("/:id/like", toggleLike);
 
 export default router;
