c9e7f9607eaf3aee87a9f8e4adeca29abf932c12
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 18:18:12 +0500

    Update home_api.dart

--- lib/home/home_api.dart
+++ lib/home/home_api.dart
@@ -1,26 +1,33 @@
-import '../core/api.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../core/constants.dart';
+import 'post_model.dart';
 
 class HomeApi {
-  static Future<List<dynamic>> fetchFeed() async {
-    final res = await Api.get('/posts/feed');
-    return res;
+  static Future<List<Post>> fetchFeed() async {
+    final res = await http.get(
+      Uri.parse('${Constants.baseUrl}/posts'),
+    );
+
+    if (res.statusCode != 200) {
+      throw Exception('Failed to load feed');
+    }
+
+    final List data = jsonDecode(res.body);
+    return data.map((e) => Post.fromJson(e)).toList();
   }
 
-  static Future<int> toggleLike(String postId) async {
-    final res = await Api.post('/posts/$postId/like');
-    return res['likes'];
+  static Future<int> like(String postId) async {
+    final res = await http.post(
+      Uri.parse('${Constants.baseUrl}/posts/$postId/like'),
+    );
+    return jsonDecode(res.body)['likes'];
   }
 
   static Future<bool> toggleSave(String postId) async {
-    final res = await Api.post('/posts/$postId/save');
-    return res['saved'];
-  }
-
-  static Future<List<dynamic>> getComments(String postId) async {
-    return await Api.get('/posts/$postId/comments');
-  }
-
-  static Future<void> addComment(String postId, String text) async {
-    await Api.post('/posts/$postId/comments', body: {'text': text});
+    final res = await http.post(
+      Uri.parse('${Constants.baseUrl}/posts/$postId/save'),
+    );
+    return jsonDecode(res.body)['saved'];
   }
 }
