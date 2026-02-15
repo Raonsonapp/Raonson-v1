826e9147825c2210b0ca5e8eeafd5d6d5fdfb1c6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 10:26:23 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
-import 'auth/auth_gate.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'core/firebase_options.dart';
+import 'firebase_options.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const RaonsonApp());
-}
-
-class RaonsonApp extends StatelessWidget {
-  const RaonsonApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthGate(), // ✅ ҚАДАМИ 5 ДАР ИН ҶО
-    );
-  }
 }
