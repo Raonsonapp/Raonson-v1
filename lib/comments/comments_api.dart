5e406ca91aef99929ebfc115af76414b4e0873cc
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 19:39:34 +0500

    Update comments_api.dart

--- lib/comments/comments_api.dart
+++ lib/comments/comments_api.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import '../core/constants.dart';
 
 class CommentApi {
-  // GET COMMENTS
   static Future<List<dynamic>> fetchComments(String postId) async {
     final res = await http.get(
       Uri.parse('${Constants.baseUrl}/comments/$postId'),
@@ -17,12 +16,18 @@ class CommentApi {
     return jsonDecode(res.body);
   }
 
-  // ADD COMMENT
   static Future<void> addComment(String postId, String text) async {
-    await http.post(
+    final res = await http.post(
       Uri.parse('${Constants.baseUrl}/comments/$postId'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'text': text}),
+      body: jsonEncode({
+        'user': 'raonson', // MVP
+        'text': text,
+      }),
     );
+
+    if (res.statusCode != 200) {
+      throw Exception('Failed to add comment');
+    }
   }
 }
