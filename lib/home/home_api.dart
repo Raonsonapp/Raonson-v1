70db3329ab45d5e5effabdcd99a33d187fd144d0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 19:19:20 +0500

    Update home_api.dart

--- lib/home/home_api.dart
+++ lib/home/home_api.dart
@@ -3,25 +3,41 @@ import 'package:http/http.dart' as http;
 import '../core/constants.dart';
 
 class HomeApi {
-  // GET FEED
+  static const _headers = {'Content-Type': 'application/json'};
+
+  // FEED
   static Future<List<dynamic>> fetchFeed() async {
     final res = await http.get(
-      Uri.parse('${Constants.baseUrl}/feed'),
-      headers: {'Content-Type': 'application/json'},
+      Uri.parse('${Constants.baseUrl}/posts'),
     );
-
     if (res.statusCode != 200) {
       throw Exception('Failed to load feed');
     }
-
     return jsonDecode(res.body);
   }
 
-  // ❤️ LIKE POST
-  static Future<void> likePost(String postId) async {
-    await http.post(
+  // LIKE
+  static Future<int> likePost(String postId) async {
+    final res = await http.post(
       Uri.parse('${Constants.baseUrl}/posts/$postId/like'),
-      headers: {'Content-Type': 'application/json'},
+      headers: _headers,
     );
+    if (res.statusCode != 200) {
+      throw Exception('Like failed');
+    }
+    return jsonDecode(res.body)['likes'];
+  }
+
+  // SAVE / UNSAVE
+  static Future<bool> toggleSave(String postId) async {
+    final res = await http.post(
+      Uri.parse('${Constants.baseUrl}/posts/$postId/save'),
+      headers: _headers,
+      body: jsonEncode({'user': 'raonson'}),
+    );
+    if (res.statusCode != 200) {
+      throw Exception('Save failed');
+    }
+    return jsonDecode(res.body)['saved'] == true;
   }
 }
