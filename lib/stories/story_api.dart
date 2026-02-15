6e5e78b558ad8b20c9bc1d1c86a9c42f510936d0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 20:11:19 +0500

    Update story_api.dart

--- lib/stories/story_api.dart
+++ lib/stories/story_api.dart
@@ -1,36 +1,53 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../core/constants.dart';
 import 'story_model.dart';
 
-class StoryApi {
-  static const String baseUrl = 'https://YOUR_RENDER_BACKEND_URL';
-
-  /// GET STORIES (grouped by user)
+class StoriesApi {
+  /// GET stories (grouped by user)
   static Future<Map<String, List<Story>>> fetchStories() async {
-    final res = await http.get(Uri.parse('$baseUrl/stories'));
+    final res = await http.get(
+      Uri.parse('${Constants.baseUrl}/stories'),
+    );
 
     if (res.statusCode != 200) {
       throw Exception('Failed to load stories');
     }
 
-    final data = jsonDecode(res.body) as Map<String, dynamic>;
-    final Map<String, List<Story>> result = {};
+    final Map data = jsonDecode(res.body);
 
+    final Map<String, List<Story>> result = {};
     data.forEach((user, list) {
-      result[user] = (list as List)
-          .map((e) => Story.fromJson(e))
-          .toList();
+      result[user] =
+          (list as List).map((e) => Story.fromJson(e)).toList();
     });
 
     return result;
   }
 
-  /// VIEW STORY
-  static Future<void> viewStory(String storyId, String user) async {
+  /// VIEW story
+  static Future<void> viewStory(String id, String user) async {
     await http.post(
-      Uri.parse('$baseUrl/stories/$storyId/view'),
+      Uri.parse('${Constants.baseUrl}/stories/$id/view'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({'user': user}),
+    );
+  }
+
+  /// CREATE story
+  static Future<void> createStory({
+    required String user,
+    required String mediaUrl,
+    required String mediaType,
+  }) async {
+    await http.post(
+      Uri.parse('${Constants.baseUrl}/stories'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'user': user,
+        'mediaUrl': mediaUrl,
+        'mediaType': mediaType,
+      }),
     );
   }
 }
