95d0d7c6fc64a3596c61c5ec850757351ca94dc1
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 08:49:18 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'auth/auth_gate.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const RaonsonApp());
+}
+
+class RaonsonApp extends StatelessWidget {
+  const RaonsonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthGate(),
+    );
+  }
 }
