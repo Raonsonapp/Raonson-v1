ee3299503614250b9d18cf8d82f36f7f5a6eef95
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 17:03:36 +0500

    Update upload_api.dart

--- lib/upload/upload_api.dart
+++ lib/upload/upload_api.dart
@@ -1,26 +1,29 @@
-import 'dart:convert';
 import '../core/api.dart';
 
 class UploadApi {
   static Future<void> uploadPost({
     required String caption,
-    required String mediaUrl,
-    required String mediaType, // image | video
+    required List<Map<String, String>> media,
   }) async {
-    await Api.post('/posts', body: {
-      'caption': caption,
-      'mediaUrl': mediaUrl,
-      'mediaType': mediaType,
-    });
+    await Api.post(
+      '/posts',
+      body: {
+        'caption': caption,
+        'media': media,
+      },
+    );
   }
 
   static Future<void> uploadReel({
     required String caption,
     required String videoUrl,
   }) async {
-    await Api.post('/reels', body: {
-      'caption': caption,
-      'videoUrl': videoUrl,
-    });
+    await Api.post(
+      '/reels',
+      body: {
+        'caption': caption,
+        'videoUrl': videoUrl,
+      },
+    );
   }
 }
