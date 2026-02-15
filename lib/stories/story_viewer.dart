e5e45a9a2c47058fabcf39f3c94eab5747354d2a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 22:54:48 +0500

    Update story_viewer.dart

--- lib/stories/story_viewer.dart
+++ lib/stories/story_viewer.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'story_model.dart';
+
+import 'stories_bar.dart';
 import 'story_api.dart';
 
 class StoryViewer extends StatefulWidget {
+  final String user;
   final List<Story> stories;
 
-  const StoryViewer({super.key, required this.stories});
+  const StoryViewer({super.key, required this.user, required this.stories});
 
   @override
   State<StoryViewer> createState() => _StoryViewerState();
@@ -14,26 +16,24 @@ class StoryViewer extends StatefulWidget {
 
 class _StoryViewerState extends State<StoryViewer> {
   int index = 0;
-  Timer? timer;
   double progress = 0;
+  Timer? timer;
 
   @override
   void initState() {
     super.initState();
-    view();
     start();
-  }
-
-  void view() {
-    StoryApi.viewStory(widget.stories[index].id, 'raonson');
   }
 
   void start() {
     timer?.cancel();
     progress = 0;
-    timer = Timer.periodic(const Duration(milliseconds: 50), (t) {
+
+    StoryApi.viewStory(widget.stories[index].id, 'raonson');
+
+    timer = Timer.periodic(const Duration(milliseconds: 50), (_) {
       setState(() {
-        progress += 0.02;
+        progress += 0.01;
         if (progress >= 1) next();
       });
     });
@@ -43,7 +43,6 @@ class _StoryViewerState extends State<StoryViewer> {
     timer?.cancel();
     if (index < widget.stories.length - 1) {
       setState(() => index++);
-      view();
       start();
     } else {
       Navigator.pop(context);
@@ -58,38 +57,34 @@ class _StoryViewerState extends State<StoryViewer> {
 
   @override
   Widget build(BuildContext context) {
-    final story = widget.stories[index];
+    final s = widget.stories[index];
 
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.network(story.mediaUrl, fit: BoxFit.cover),
-          ),
-          Positioned(
-            top: 40,
-            left: 12,
-            right: 12,
-            child: LinearProgressIndicator(
-              value: progress,
-              backgroundColor: Colors.white24,
-              valueColor:
-                  const AlwaysStoppedAnimation<Color>(Colors.white),
+      body: GestureDetector(
+        onTap: next,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.network(s.mediaUrl, fit: BoxFit.cover),
             ),
-          ),
-          Positioned(
-            top: 60,
-            left: 16,
-            child: Text(
-              story.user,
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+            Positioned(
+              top: 40,
+              left: 12,
+              right: 12,
+              child: LinearProgressIndicator(value: progress),
+            ),
+            Positioned(
+              top: 60,
+              left: 16,
+              child: Text(
+                widget.user,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
