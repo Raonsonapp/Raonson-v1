e5a5d6c5a9c5b3469628cebcfc6bfbdb453d4ee0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 20:39:27 +0500

    Update notification_model.dart

--- lib/notifications/notification_model.dart
+++ lib/notifications/notification_model.dart
@@ -2,12 +2,14 @@ class AppNotification {
   final String id;
   final String from;
   final String type;
+  final String? postId;
   final bool seen;
 
   AppNotification({
     required this.id,
     required this.from,
     required this.type,
+    this.postId,
     required this.seen,
   });
 
@@ -16,6 +18,7 @@ class AppNotification {
       id: json['id'],
       from: json['from'],
       type: json['type'],
+      postId: json['postId'],
       seen: json['seen'] ?? false,
     );
   }
