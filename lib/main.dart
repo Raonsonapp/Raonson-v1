dc0cea3b840b3f422868d3ae622be6f4ac9f7998
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 11:24:02 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
-import 'core/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // ✅ Android-only Firebase init (NO firebase_options.dart)
+  await Firebase.initializeApp();
 
   runApp(const RaonsonApp());
 }
