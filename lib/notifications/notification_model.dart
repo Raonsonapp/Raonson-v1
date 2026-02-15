827accf737cbe94ee48df8222a51930295ed248a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 22:17:44 +0500

    Update notification_model.dart

--- lib/notifications/notification_model.dart
+++ lib/notifications/notification_model.dart
@@ -1,25 +1,31 @@
 class AppNotification {
   final String id;
   final String from;
-  final String type;
+  final String to;
+  final String type; // like | follow | comment
   final String? postId;
   final bool seen;
+  final String createdAt;
 
   AppNotification({
     required this.id,
     required this.from,
+    required this.to,
     required this.type,
     this.postId,
     required this.seen,
+    required this.createdAt,
   });
 
   factory AppNotification.fromJson(Map<String, dynamic> json) {
     return AppNotification(
       id: json['id'],
       from: json['from'],
+      to: json['to'],
       type: json['type'],
       postId: json['postId'],
       seen: json['seen'] ?? false,
+      createdAt: json['createdAt'],
     );
   }
 }
