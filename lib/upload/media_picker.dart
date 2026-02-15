3a8d7383268e3c1db0ab58b6a0fee04ec877d79e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 18:05:42 +0500

    Update media_picker.dart

--- lib/upload/media_picker.dart
+++ lib/upload/media_picker.dart
@@ -4,16 +4,14 @@ import 'package:image_picker/image_picker.dart';
 class PickedMedia {
   final File file;
   final bool isVideo;
-
   PickedMedia({required this.file, required this.isVideo});
 }
 
 class MediaPicker {
   static final ImagePicker _picker = ImagePicker();
 
-  static Future<List<PickedMedia>> pickMultiple() async {
-    final List<XFile> files = await _picker.pickMultipleMedia();
-
+  static Future<List<PickedMedia>> pick() async {
+    final files = await _picker.pickMultipleMedia();
     return files.map((f) {
       final isVideo = f.path.endsWith('.mp4') || f.path.endsWith('.mov');
       return PickedMedia(file: File(f.path), isVideo: isVideo);
