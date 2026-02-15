5c1bd6c50f46a2201636b21c8d3d8f9c3d794334
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 19:18:39 +0500

    Update post_model.dart

--- lib/home/post_model.dart
+++ lib/home/post_model.dart
@@ -1,29 +1,38 @@
 class Post {
   final String id;
-  final String username;
-  final String imageUrl;
+  final String user;
+  final String mediaUrl;
+  final String mediaType; // image | video
   final String caption;
 
   int likes;
+  int comments;
   bool liked;
+  bool saved;
 
   Post({
     required this.id,
-    required this.username,
-    required this.imageUrl,
+    required this.user,
+    required this.mediaUrl,
+    required this.mediaType,
     required this.caption,
-    this.likes = 0,
+    required this.likes,
+    required this.comments,
     this.liked = false,
+    this.saved = false,
   });
 
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
       id: json['id'].toString(),
-      username: json['user'] ?? 'user',
-      imageUrl: json['imageUrl'],
+      user: json['user'] ?? 'user',
+      mediaUrl: json['mediaUrl'],
+      mediaType: json['mediaType'], // image | video
       caption: json['caption'] ?? '',
       likes: json['likes'] ?? 0,
+      comments: json['comments'] ?? 0,
       liked: json['liked'] ?? false,
+      saved: json['saved'] ?? false,
     );
   }
 }
