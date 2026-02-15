e21d8520e942a3300cb031200c8198348378d359
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 18:17:45 +0500

    Update post_model.dart

--- lib/home/post_model.dart
+++ lib/home/post_model.dart
@@ -1,11 +1,31 @@
 class Post {
-  final String username;
-  final String imageUrl;
+  final String id;
+  final String user;
+  final String caption;
+  final List<String> media;
+  int likes;
   bool liked;
+  bool saved;
 
   Post({
-    required this.username,
-    required this.imageUrl,
+    required this.id,
+    required this.user,
+    required this.caption,
+    required this.media,
+    required this.likes,
     this.liked = false,
+    this.saved = false,
   });
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      user: json['user'],
+      caption: json['caption'] ?? '',
+      media: List<String>.from(json['media'] ?? []),
+      likes: json['likes'] ?? 0,
+      liked: json['liked'] ?? false,
+      saved: json['saved'] ?? false,
+    );
+  }
 }
