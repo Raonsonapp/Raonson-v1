7396299df71c773b64c9f3e14c3606cc8ef417f6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 11:14:06 +0500

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'auth/auth_gate.dart';
 import 'reels/reels_screen.dart';
 
+/// 🔹 ROOT APP
+class RaonsonApp extends StatelessWidget {
+  const RaonsonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const AuthGate(),
+    );
+  }
+}
+
+/// 🔹 MAIN NAVIGATION (after login)
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
 
@@ -12,43 +28,32 @@ class _MainNavigationState extends State<MainNavigation> {
   int index = 1;
 
   final pages = const [
-    Center(child: Text('')),
+    Center(child: Text('Home', style: TextStyle(color: Colors.white))),
     ReelsScreen(),
-    Center(child: Text('')),
-    Center(child: Text('')),
+    Center(child: Text('Search', style: TextStyle(color: Colors.white))),
+    Center(child: Text('Profile', style: TextStyle(color: Colors.white))),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: pages[index],
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.blueAccent.withOpacity(0.5),
-              blurRadius: 20,
-              offset: const Offset(0, -4),
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          currentIndex: index,
-          onTap: (i) => setState(() => index = i),
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.black,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white54,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.play_circle), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (i) => setState(() => index = i),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white54,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.play_circle), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+        ],
       ),
     );
   }
