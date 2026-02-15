04ef022d2a9611c6c01da85cf34ce1ba3d5b839e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 19:22:50 +0500

    Update story_viewer.dart

--- lib/stories/story_viewer.dart
+++ lib/stories/story_viewer.dart
@@ -1,74 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import 'story_model.dart';
+import 'story_api.dart';
 
 class StoryViewer extends StatefulWidget {
-  final List<Story> stories;
-  final int initialIndex;
-
-  const StoryViewer({
-    super.key,
-    required this.stories,
-    required this.initialIndex,
-  });
+  final Story story;
+  const StoryViewer({super.key, required this.story});
 
   @override
   State<StoryViewer> createState() => _StoryViewerState();
 }
 
 class _StoryViewerState extends State<StoryViewer> {
-  late PageController controller;
-  late int index;
+  VideoPlayerController? controller;
 
   @override
   void initState() {
     super.initState();
-    index = widget.initialIndex;
-    controller = PageController(initialPage: index);
+
+    StoryApi.markViewed(widget.story.id);
+
+    if (widget.story.mediaType == 'video') {
+      controller = VideoPlayerController.network(widget.story.mediaUrl)
+        ..initialize().then((_) {
+          setState(() {});
+          controller!.play();
+        });
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          PageView.builder(
-            controller: controller,
-            itemCount: widget.stories.length,
-            onPageChanged: (i) => setState(() => index = i),
-            itemBuilder: (_, i) {
-              return Image.network(
-                widget.stories[i].imageUrl,
-                fit: BoxFit.cover,
-                width: double.infinity,
-                height: double.infinity,
-              );
-            },
-          ),
-
-          // ❌ CLOSE
-          Positioned(
-            top: 40,
-            right: 20,
-            child: IconButton(
-              icon: const Icon(Icons.close, color: Colors.white),
-              onPressed: () => Navigator.pop(context),
-            ),
-          ),
-
-          // USERNAME
-          Positioned(
-            top: 40,
-            left: 20,
-            child: Text(
-              widget.stories[index].username,
-              style: const TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
+      body: GestureDetector(
+        onVerticalDragDown: (_) => Navigator.pop(context),
+        child: Center(
+          child: widget.story.mediaType == 'image'
+              ? Image.network(widget.story.mediaUrl)
+              : controller != null && controller!.value.isInitialized
+                  ? VideoPlayer(controller!)
+                  : const CircularProgressIndicator(),
+        ),
       ),
     );
   }
