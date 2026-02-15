4db92bc20a9e4d8244303bc12abeb15bd6af2c9f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 19:37:04 +0500

    Update post_model.dart

--- lib/home/post_model.dart
+++ lib/home/post_model.dart
@@ -1,38 +1,44 @@
 class Post {
   final String id;
   final String user;
-  final String mediaUrl;
-  final String mediaType; // image | video
   final String caption;
+  final List<PostMedia> media;
 
   int likes;
-  int comments;
   bool liked;
-  bool saved;
 
   Post({
     required this.id,
     required this.user,
-    required this.mediaUrl,
-    required this.mediaType,
     required this.caption,
+    required this.media,
     required this.likes,
-    required this.comments,
     this.liked = false,
-    this.saved = false,
   });
 
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
       id: json['id'].toString(),
-      user: json['user'] ?? 'user',
-      mediaUrl: json['mediaUrl'],
-      mediaType: json['mediaType'], // image | video
+      user: json['user'],
       caption: json['caption'] ?? '',
       likes: json['likes'] ?? 0,
-      comments: json['comments'] ?? 0,
-      liked: json['liked'] ?? false,
-      saved: json['saved'] ?? false,
+      media: (json['media'] as List)
+          .map((e) => PostMedia.fromJson(e))
+          .toList(),
+    );
+  }
+}
+
+class PostMedia {
+  final String url;
+  final String type; // image | video
+
+  PostMedia({required this.url, required this.type});
+
+  factory PostMedia.fromJson(Map<String, dynamic> json) {
+    return PostMedia(
+      url: json['url'],
+      type: json['type'],
     );
   }
 }
