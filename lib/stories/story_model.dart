5cb167b656695e5b3b8c5d7fe7f2efbfd88dacbf
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 19:21:12 +0500

    Update story_model.dart

--- lib/stories/story_model.dart
+++ lib/stories/story_model.dart
@@ -1,13 +1,25 @@
 class Story {
   final String id;
-  final String username;
-  final String imageUrl;
-  final bool isMe;
+  final String user;
+  final String mediaUrl;
+  final String mediaType; // image | video
+  bool viewed;
 
   Story({
     required this.id,
-    required this.username,
-    required this.imageUrl,
-    this.isMe = false,
+    required this.user,
+    required this.mediaUrl,
+    required this.mediaType,
+    this.viewed = false,
   });
+
+  factory Story.fromJson(Map<String, dynamic> json) {
+    return Story(
+      id: json['id'].toString(),
+      user: json['user'],
+      mediaUrl: json['mediaUrl'],
+      mediaType: json['mediaType'],
+      viewed: json['viewed'] ?? false,
+    );
+  }
 }
