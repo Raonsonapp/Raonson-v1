386728d1782745454499c2eeb7496d08cb44c9f4
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 10:31:10 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'app.dart';
+import 'auth/auth_gate.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(const RaonsonApp());
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: AuthGate(),
+  ));
 }
