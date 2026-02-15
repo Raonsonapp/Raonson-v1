1781037a808a5780e01df9e24a6e061556af1ed4
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-15 22:53:31 +0500

    Update story_api.dart

--- lib/stories/story_api.dart
+++ lib/stories/story_api.dart
@@ -1,49 +1,25 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '../core/constants.dart';
+import '../core/api.dart';
 
 class StoryApi {
-  // GET STORIES (24h, grouped by user)
-  static Future<Map<String, List<dynamic>>> fetchStories() async {
-    final res = await http.get(
-      Uri.parse('${Constants.baseUrl}/stories'),
-      headers: {'Content-Type': 'application/json'},
-    );
-
-    if (res.statusCode != 200) {
-      throw Exception('Failed to load stories');
-    }
-
-    final Map<String, dynamic> data = jsonDecode(res.body);
-
-    return data.map(
-      (key, value) => MapEntry(key, List<dynamic>.from(value)),
-    );
+  static Future<Map<String, dynamic>> fetchStories() async {
+    return await Api.get('/stories');
   }
 
-  // CREATE STORY
-  static Future<void> uploadStory({
+  static Future<void> createStory({
     required String user,
     required String mediaUrl,
     required String mediaType, // image | video
   }) async {
-    await http.post(
-      Uri.parse('${Constants.baseUrl}/stories'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'user': user,
-        'mediaUrl': mediaUrl,
-        'mediaType': mediaType,
-      }),
-    );
+    await Api.post('/stories', body: {
+      'user': user,
+      'mediaUrl': mediaUrl,
+      'mediaType': mediaType,
+    });
   }
 
-  // VIEW STORY
   static Future<void> viewStory(String storyId, String user) async {
-    await http.post(
-      Uri.parse('${Constants.baseUrl}/stories/$storyId/view'),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'user': user}),
-    );
+    await Api.post('/stories/$storyId/view', body: {
+      'user': user,
+    });
   }
 }
