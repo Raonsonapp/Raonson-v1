9bd40a9b5685e5e29989ed1d0e4b4f230b41e365
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 20:20:34 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import followRoutes from "./routes/follow.routes.js";
 import profileRoutes from "./routes/profile.routes.js";
+import searchRoutes from "./routes/search.routes.js";
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.use("/posts", postRoutes);
 app.use("/comments", commentRoutes);
 app.use("/follow", followRoutes);
 app.use("/profile", profileRoutes);
+app.use("/search", searchRoutes);
 
 export default app;
