f83f3b75ffa29ad7acb0b9726089ed0ca478b16e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 20:39:08 +0500

    Update chat_api.dart

--- lib/chat/chat_api.dart
+++ lib/chat/chat_api.dart
@@ -1,24 +1,22 @@
-import 'dart:convert';
 import '../core/api.dart';
 
 class ChatApi {
-  /// GET /chats
-  static Future<List<dynamic>> getChats() async {
+  static Future<List<dynamic>> fetchChats() async {
     final res = await Api.get('/chats');
-    return jsonDecode(res.body);
+    return res;
   }
 
-  /// GET /chats/:id/messages
-  static Future<List<dynamic>> getMessages(String chatId) async {
-    final res = await Api.get('/chats/$chatId/messages');
-    return jsonDecode(res.body);
+  static Future<void> sendMessage({
+    required String chatId,
+    required String text,
+  }) async {
+    await Api.post('/chats/send', {
+      'chatId': chatId,
+      'text': text,
+    });
   }
 
-  /// POST /chats/:id/messages
-  static Future<void> sendMessage(String chatId, String text) async {
-    await Api.post(
-      '/chats/$chatId/messages',
-      body: {'text': text},
-    );
+  static Future<void> markSeen(String chatId) async {
+    await Api.post('/chats/$chatId/seen', {});
   }
 }
