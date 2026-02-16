09a5e28c89dc964d42d1a223e461e4b168825bfd
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 23:22:05 +0500

    Update responseFormatter.js

--- backend/src/core/responseFormatter.js
+++ backend/src/core/responseFormatter.js
@@ -1,7 +1,14 @@
-export function formatResponse(success, message, data) {
-  return {
-    success,
-    message,
+export function success(res, data, meta = {}) {
+  res.json({
+    success: true,
     data,
-  };
+    meta,
+  });
+}
+
+export function failure(res, status, message) {
+  res.status(status).json({
+    success: false,
+    error: message,
+  });
 }
