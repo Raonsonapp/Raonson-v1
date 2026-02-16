bf2b1d3e13c61af2c54af65c755dfa3b10c29d2c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 21:06:31 +0500

    Update chat_list_screen.dart

--- lib/chat/chat_list_screen.dart
+++ lib/chat/chat_list_screen.dart
@@ -11,7 +11,6 @@ class ChatListScreen extends StatefulWidget {
 
 class _ChatListScreenState extends State<ChatListScreen> {
   List chats = [];
-  bool loading = true;
 
   @override
   void initState() {
@@ -20,11 +19,8 @@ class _ChatListScreenState extends State<ChatListScreen> {
   }
 
   Future<void> load() async {
-    final data = await ChatApi.fetchChats();
-    setState(() {
-      chats = data;
-      loading = false;
-    });
+    chats = await ChatApi.getChats();
+    setState(() {});
   }
 
   @override
@@ -35,49 +31,28 @@ class _ChatListScreenState extends State<ChatListScreen> {
         backgroundColor: Colors.black,
         title: const Text('Messages'),
       ),
-      body: loading
-          ? const Center(child: CircularProgressIndicator(color: Colors.white))
-          : ListView.builder(
-              itemCount: chats.length,
-              itemBuilder: (_, i) {
-                final c = chats[i];
-                final last = c['lastMessage'];
-                final seen = last?['seen'] == true;
-
-                return ListTile(
-                  leading: const CircleAvatar(
-                    backgroundColor: Colors.orange,
-                    child: Icon(Icons.person, color: Colors.black),
-                  ),
-                  title: Text(
-                    c['users']
-                        .firstWhere((u) => u['_id'] != 'me')['username'],
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  subtitle: Text(
-                    last?['text'] ?? '',
-                    style: TextStyle(
-                      color: seen ? Colors.white54 : Colors.white,
-                      fontWeight:
-                          seen ? FontWeight.normal : FontWeight.bold,
-                    ),
-                  ),
-                  trailing: !seen
-                      ? const Icon(Icons.circle,
-                          color: Colors.blue, size: 10)
-                      : null,
-                  onTap: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => ChatScreen(chat: c),
-                      ),
-                    );
-                    load();
-                  },
-                );
-              },
+      body: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (_, i) {
+          final chat = chats[i];
+          final user = chat['members'][1];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(user['avatar']),
             ),
+            title: Text(user['username'],
+                style: const TextStyle(color: Colors.white)),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ChatScreen(chatId: chat['_id']),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
