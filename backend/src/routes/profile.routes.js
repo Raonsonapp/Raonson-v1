4cd5615d143025e7ccff551e3f0c0e651f6813e2
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 22:25:30 +0500

    Update profile.routes.js

--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { viewProfile } from "../controllers/profile.controller.js";
+import { getProfile } from "../controllers/profile.controller.js";
 
 const router = express.Router();
 
-router.get("/:username", authMiddleware, viewProfile);
+router.get("/:id", authMiddleware, getProfile);
 
 export default router;
