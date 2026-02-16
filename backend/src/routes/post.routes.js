38e0e26fbeef3ccbc2837c47205888ee83e1e447
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 19:19:38 +0500

    Update post.routes.js

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createPost, getFeed } from "../controllers/post.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", authMiddleware, createPost);
+// ❌ authMiddleware-ро гирифтем (MVP)
+router.post("/", createPost);
 router.get("/", getFeed);
 
 export default router;
