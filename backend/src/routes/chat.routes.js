ffe687e33b3aa8544f80691356e550d518eb20e0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 21:03:32 +0500

    Update chat.routes.js

--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -1,15 +1,10 @@
 import express from "express";
-import {
-  getChats,
-  sendMessage,
-  markSeen,
-} from "../controllers/chat.controller.js";
+import { getChats, sendMessage } from "../controllers/chat.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getChats);
 router.post("/send", authMiddleware, sendMessage);
-router.post("/:id/seen", authMiddleware, markSeen);
 
 export default router;
