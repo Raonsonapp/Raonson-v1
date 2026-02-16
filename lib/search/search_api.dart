6be4123d382cc728dd43ea9cfd3cf25724189215
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 20:51:46 +0500

    Update search_api.dart

--- lib/search/search_api.dart
+++ lib/search/search_api.dart
@@ -1,9 +1,19 @@
-import 'dart:convert';
 import '../core/api.dart';
+import 'dart:convert';
 
 class SearchApi {
-  static Future<Map<String, dynamic>> search(String q) async {
-    final res = await Api.get('/search?q=$q');
+  static Future<List<dynamic>> users(String q) async {
+    final res = await Api.get('/search/users?q=$q');
+    return jsonDecode(res.body);
+  }
+
+  static Future<List<dynamic>> posts(String q) async {
+    final res = await Api.get('/search/posts?q=$q');
+    return jsonDecode(res.body);
+  }
+
+  static Future<List<dynamic>> reels() async {
+    final res = await Api.get('/search/reels');
     return jsonDecode(res.body);
   }
 }
