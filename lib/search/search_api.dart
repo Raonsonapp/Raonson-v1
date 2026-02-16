1db40100499ada646e5aa8e8279b1a008063f922
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 20:21:14 +0500

    Update search_api.dart

--- lib/search/search_api.dart
+++ lib/search/search_api.dart
@@ -4,9 +4,6 @@ import '../core/api.dart';
 class SearchApi {
   static Future<Map<String, dynamic>> search(String q) async {
     final res = await Api.get('/search?q=$q');
-    if (res.statusCode == 200) {
-      return jsonDecode(res.body);
-    }
-    throw Exception('Search failed');
+    return jsonDecode(res.body);
   }
 }
