5dd4c87ab10f85eb4576f609069da2a70427af88
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 19:44:49 +0500

    Update comment.model.js

--- backend/src/models/comment.model.js
+++ backend/src/models/comment.model.js
@@ -2,22 +2,34 @@ import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-
-    targetType: {
-      type: String,
-      enum: ["post", "reel"],
-      required: true,
-    },
-
-    targetId: {
+    post: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+      index: true,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
 
-    text: { type: String, required: true },
+    text: {
+      type: String,
+      required: true,
+      maxlength: 1000,
+    },
+
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-export default mongoose.model("Comment", commentSchema);
+export const Comment = mongoose.model("Comment", commentSchema);
