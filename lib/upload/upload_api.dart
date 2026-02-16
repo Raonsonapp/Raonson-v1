1b85590f3302675b88aa714af6bca1cd99e9bcfc
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 17:50:04 +0500

    Update upload_api.dart

--- lib/upload/upload_api.dart
+++ lib/upload/upload_api.dart
@@ -2,12 +2,14 @@ import '../core/api.dart';
 
 class UploadApi {
   static Future<void> createPost({
+    required String user,
     required String caption,
     required List<Map<String, String>> media,
   }) async {
     await Api.post(
       '/posts',
       {
+        'user': user,
         'caption': caption,
         'media': media,
       },
@@ -15,12 +17,14 @@ class UploadApi {
   }
 
   static Future<void> createStory({
+    required String user,
     required String mediaUrl,
     required String mediaType,
   }) async {
     await Api.post(
       '/stories',
       {
+        'user': user,
         'mediaUrl': mediaUrl,
         'mediaType': mediaType,
       },
