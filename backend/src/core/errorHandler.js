3159b08344bb2923cac3fa89bfcb4a5ab4c69766
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 23:00:30 +0500

    Update errorHandler.js

--- backend/src/core/errorHandler.js
+++ backend/src/core/errorHandler.js
@@ -1,8 +1,8 @@
 export function errorHandler(err, req, res, next) {
-  console.error(err);
+  const status = err.status || 500;
 
-  res.status(err.status || 500).json({
+  res.status(status).json({
     success: false,
-    message: err.message || "Server error",
+    message: err.message || "Internal Server Error",
   });
 }
