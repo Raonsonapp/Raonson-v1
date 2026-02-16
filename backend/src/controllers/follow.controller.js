5351322be60e9c9cc6b22ee39f4884e8cda2e904
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 19:48:10 +0500

    Update follow.controller.js

--- backend/src/controllers/follow.controller.js
+++ backend/src/controllers/follow.controller.js
@@ -1,35 +1,72 @@
-import Follow from "../models/follow.model.js";
+import { Follow } from "../models/follow.model.js";
+import { User } from "../models/user.model.js";
+import { addNotification } from "./notification.controller.js";
 
-// FOLLOW / UNFOLLOW (toggle)
-export const toggleFollow = async (req, res) => {
-  const followerId = req.user.id;
-  const { userId } = req.params;
+// FOLLOW / UNFOLLOW
+export async function toggleFollow(req, res) {
+  const targetUserId = req.params.userId;
+  const myId = req.user._id;
 
-  if (followerId === userId) {
+  if (targetUserId === myId.toString()) {
     return res.status(400).json({ error: "Cannot follow yourself" });
   }
 
-  const existing = await Follow.findOne({ followerId, followingId: userId });
+  const existing = await Follow.findOne({
+    from: myId,
+    to: targetUserId,
+  });
 
   if (existing) {
+    // UNFOLLOW
     await existing.deleteOne();
+
+    await User.findByIdAndUpdate(myId, {
+      $inc: { followingCount: -1 },
+    });
+    await User.findByIdAndUpdate(targetUserId, {
+      $inc: { followersCount: -1 },
+    });
+
     return res.json({ following: false });
-  } else {
-    await Follow.create({ followerId, followingId: userId });
-    return res.json({ following: true });
   }
-};
+
+  // FOLLOW
+  await Follow.create({
+    from: myId,
+    to: targetUserId,
+  });
+
+  await User.findByIdAndUpdate(myId, {
+    $inc: { followingCount: 1 },
+  });
+  await User.findByIdAndUpdate(targetUserId, {
+    $inc: { followersCount: 1 },
+  });
+
+  // notification
+  addNotification({
+    to: targetUserId,
+    from: myId,
+    type: "follow",
+  });
+
+  res.json({ following: true });
+}
 
 // GET FOLLOWERS
-export const getFollowers = async (req, res) => {
-  const { userId } = req.params;
-  const followers = await Follow.find({ followingId: userId });
-  res.json(followers);
-};
+export async function getFollowers(req, res) {
+  const list = await Follow.find({ to: req.params.userId })
+    .populate("from", "username avatar verified")
+    .limit(100);
+
+  res.json(list);
+}
 
 // GET FOLLOWING
-export const getFollowing = async (req, res) => {
-  const { userId } = req.params;
-  const following = await Follow.find({ followerId: userId });
-  res.json(following);
-};
+export async function getFollowing(req, res) {
+  const list = await Follow.find({ from: req.params.userId })
+    .populate("to", "username avatar verified")
+    .limit(100);
+
+  res.json(list);
+}
