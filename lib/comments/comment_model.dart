f5ad79b40c17fe567275e5b9f1fd68a8c3360994
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 14:34:57 +0500

    Update comment_model.dart

--- lib/comments/comment_model.dart
+++ lib/comments/comment_model.dart
@@ -1,13 +1,11 @@
 class Comment {
   final String id;
-  final String reelId;
   final String user;
   final String text;
-  final DateTime createdAt;
+  final String createdAt;
 
   Comment({
     required this.id,
-    required this.reelId,
     required this.user,
     required this.text,
     required this.createdAt,
@@ -15,11 +13,10 @@ class Comment {
 
   factory Comment.fromJson(Map<String, dynamic> json) {
     return Comment(
-      id: json['id'].toString(),
-      reelId: json['reelId'].toString(),
-      user: json['user'] ?? 'unknown',
-      text: json['text'] ?? '',
-      createdAt: DateTime.parse(json['createdAt']),
+      id: json['id'],
+      user: json['user'],
+      text: json['text'],
+      createdAt: json['createdAt'],
     );
   }
 }
