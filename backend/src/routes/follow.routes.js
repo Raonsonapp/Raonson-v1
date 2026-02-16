781c69b774e6f9bd8caf2b3a47aad50df9ada2f6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 22:22:51 +0500

    Update follow.routes.js

--- backend/src/routes/follow.routes.js
+++ backend/src/routes/follow.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { toggleFollow } from "../controllers/follow.controller.js";
+import {
+  follow,
+  unfollow,
+  accept,
+  reject,
+} from "../controllers/follow.controller.js";
 
 const router = express.Router();
 
-router.post("/:id", authMiddleware, toggleFollow);
+router.post("/:id", authMiddleware, follow);
+router.delete("/:id", authMiddleware, unfollow);
+
+router.post("/requests/:id/accept", authMiddleware, accept);
+router.delete("/requests/:id/reject", authMiddleware, reject);
 
 export default router;
