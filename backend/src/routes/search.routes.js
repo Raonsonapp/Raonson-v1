2a39bbdcd7ea19b5213ec7bfbe00f2b4f7f8689f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 22:18:04 +0500

    Update search.routes.js

--- backend/src/routes/search.routes.js
+++ backend/src/routes/search.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import {
-  searchUsers,
-  searchPosts,
-  exploreReels,
+  search,
+  recent,
+  clear,
 } from "../controllers/search.controller.js";
 
 const router = express.Router();
 
-router.get("/users", searchUsers);
-router.get("/posts", searchPosts);
-router.get("/reels", exploreReels);
+router.get("/", authMiddleware, search);
+router.get("/recent", authMiddleware, recent);
+router.delete("/recent", authMiddleware, clear);
 
 export default router;
