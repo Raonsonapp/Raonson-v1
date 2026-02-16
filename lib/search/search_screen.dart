def5ae0cd9f6a7740b4b84fdeaa81f8f3967ef40
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 20:52:14 +0500

    Update search_screen.dart

--- lib/search/search_screen.dart
+++ lib/search/search_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import '../profile/profile_screen.dart';
-import '../home/post_item.dart';
 import 'search_api.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -13,25 +11,13 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final ctrl = TextEditingController();
   List users = [];
-  List posts = [];
   bool loading = false;
 
-  Future<void> onSearch(String q) async {
-    if (q.isEmpty) {
-      setState(() {
-        users = [];
-        posts = [];
-      });
-      return;
-    }
-
+  Future<void> search(String q) async {
+    if (q.isEmpty) return;
     setState(() => loading = true);
-    final res = await SearchApi.search(q);
-    setState(() {
-      users = res['users'];
-      posts = res['posts'];
-      loading = false;
-    });
+    users = await SearchApi.users(q);
+    setState(() => loading = false);
   }
 
   @override
@@ -42,88 +28,35 @@ class _SearchScreenState extends State<SearchScreen> {
         backgroundColor: Colors.black,
         title: TextField(
           controller: ctrl,
-          autofocus: true,
           style: const TextStyle(color: Colors.white),
           decoration: const InputDecoration(
             hintText: 'Search',
             hintStyle: TextStyle(color: Colors.white54),
             border: InputBorder.none,
           ),
-          onChanged: onSearch,
+          onChanged: search,
         ),
       ),
       body: loading
-          ? const Center(
-              child: CircularProgressIndicator(color: Colors.white),
-            )
-          : ListView(
-              children: [
-                if (users.isNotEmpty) _Users(users),
-                if (posts.isNotEmpty) _Posts(posts),
-              ],
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (_, i) {
+                final u = users[i];
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(u['avatar'] ?? ''),
+                  ),
+                  title: Text(
+                    u['username'],
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  trailing: u['verified'] == true
+                      ? const Icon(Icons.verified, color: Colors.blue)
+                      : null,
+                );
+              },
             ),
-    );
-  }
-}
-
-class _Users extends StatelessWidget {
-  final List users;
-  const _Users(this.users);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: users.map((u) {
-        return ListTile(
-          leading: CircleAvatar(
-            backgroundImage:
-                u['avatar'] != null ? NetworkImage(u['avatar']) : null,
-          ),
-          title: Row(
-            children: [
-              Text(u['username'],
-                  style: const TextStyle(color: Colors.white)),
-              if (u['verified'] == true)
-                const Padding(
-                  padding: EdgeInsets.only(left: 4),
-                  child:
-                      Icon(Icons.verified, color: Colors.blue, size: 16),
-                ),
-            ],
-          ),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) => ProfileScreen(userId: u['_id']),
-              ),
-            );
-          },
-        );
-      }).toList(),
-    );
-  }
-}
-
-class _Posts extends StatelessWidget {
-  final List posts;
-  const _Posts(this.posts);
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: posts.length,
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 1,
-        mainAxisSpacing: 1,
-      ),
-      itemBuilder: (_, i) {
-        final media = posts[i]['media'][0];
-        return Image.network(media['url'], fit: BoxFit.cover);
-      },
     );
   }
 }
