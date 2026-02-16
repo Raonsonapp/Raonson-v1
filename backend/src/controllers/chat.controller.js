b29277c0a8deeae5f29724c87c5e6c4f3d4f0eba
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-16 21:03:01 +0500

    Update chat.controller.js

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,15 +1,11 @@
 import { Chat } from "../models/chat.model.js";
-import { Message } from "../models/message.model.js";
-import { emitToUser } from "../socket/index.js";
 
 // GET USER CHATS
 export async function getChats(req, res) {
-  const chats = await Chat.find({ users: req.user._id })
-    .populate("users", "username avatar")
-    .populate({
-      path: "lastMessage",
-      select: "text createdAt seen sender",
-    })
+  const chats = await Chat.find({
+    members: req.user._id,
+  })
+    .populate("members", "username avatar")
     .sort({ updatedAt: -1 });
 
   res.json(chats);
@@ -19,36 +15,14 @@ export async function getChats(req, res) {
 export async function sendMessage(req, res) {
   const { chatId, text } = req.body;
 
-  const message = await Message.create({
-    chat: chatId,
+  const chat = await Chat.findById(chatId);
+  chat.messages.push({
     sender: req.user._id,
     text,
   });
 
-  await Chat.findByIdAndUpdate(chatId, {
-    lastMessage: message._id,
-    $set: { updatedAt: new Date() },
-  });
+  chat.updatedAt = new Date();
+  await chat.save();
 
-  const chat = await Chat.findById(chatId);
-  const receiver = chat.users.find(
-    u => u.toString() !== req.user._id.toString()
-  );
-
-  // 🔴 REALTIME PUSH
-  emitToUser(receiver, "new_message", {
-    chatId,
-    message,
-  });
-
-  res.json(message);
-}
-
-// MARK SEEN
-export async function markSeen(req, res) {
-  await Message.updateMany(
-    { chat: req.params.id, seen: false },
-    { seen: true }
-  );
-  res.json({ ok: true });
+  res.json(chat.messages.at(-1));
 }
