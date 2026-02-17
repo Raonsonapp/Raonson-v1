cd2059ecfe96899feb07419e74f3af964faf6888
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 20:02:45 +0500

    Delete lib/explore directory

--- lib/explore/explore_api.dart
+++
@@ -1,9 +0,0 @@
-import '../core/api.dart';
-import 'dart:convert';
-
-class ExploreApi {
-  static Future<Map<String, dynamic>> fetch() async {
-    final res = await Api.get('/explore');
-    return jsonDecode(res.body);
-  }
-}
