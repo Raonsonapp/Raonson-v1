f3503bd06a7c2557ca95c07458266ffbf2931925
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 22:31:02 +0500

    Create search_state.dart

---
+++ lib/search/search_state.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import '../models/user_model.dart';
+import '../models/post_model.dart';
+
+class SearchState {
+  final bool isLoading;
+  final String query;
+  final List<UserModel> users;
+  final List<PostModel> posts;
+  final String? error;
+
+  const SearchState({
+    required this.isLoading,
+    required this.query,
+    required this.users,
+    required this.posts,
+    this.error,
+  });
+
+  factory SearchState.initial() {
+    return const SearchState(
+      isLoading: false,
+      query: '',
+      users: [],
+      posts: [],
+    );
+  }
+
+  SearchState copyWith({
+    bool? isLoading,
+    String? query,
+    List<UserModel>? users,
+    List<PostModel>? posts,
+    String? error,
+  }) {
+    return SearchState(
+      isLoading: isLoading ?? this.isLoading,
+      query: query ?? this.query,
+      users: users ?? this.users,
+      posts: posts ?? this.posts,
+      error: error,
+    );
+  }
+}
