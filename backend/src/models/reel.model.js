f6f31f38709a298fb826228d60b70e1d2c225ec6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 07:40:28 +0500

    Update reel.model.js

--- backend/src/models/reel.model.js
+++ backend/src/models/reel.model.js
@@ -6,40 +6,14 @@ const ReelSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      index: true,
     },
 
     videoUrl: { type: String, required: true },
     caption: { type: String, default: "" },
 
-    views: {
-      type: Number,
-      default: 0,
-      index: true,
-    },
-
-    likedBy: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-
-    savedBy: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-
-    shares: { type: Number, default: 0 },
-
-    // 🔥 algorithm score
-    score: {
-      type: Number,
-      default: 0,
-      index: true,
-    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    saves: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    views: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
