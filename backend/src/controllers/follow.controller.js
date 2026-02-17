2fb10677b30a31cd314e854c1bf1f6ae85a4c2f6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 13:20:38 +0500

    Update follow.controller.js

--- backend/src/controllers/follow.controller.js
+++ backend/src/controllers/follow.controller.js
@@ -1,26 +1,79 @@
 import { User } from "../models/user.model.js";
 
-export async function toggleFollow(req, res) {
-  const target = await User.findById(req.params.userId);
-  const me = await User.findById(req.user._id);
+// FOLLOW
+export async function followUser(req, res) {
+  const targetId = req.params.id;
+  const userId = req.user.id;
 
-  const isFollowing = me.following.includes(target._id);
+  const target = await User.findById(targetId);
+  if (!target) return res.status(404).json({ message: "User not found" });
 
-  if (isFollowing) {
-    me.following.pull(target._id);
-    target.followers.pull(me._id);
-  } else {
-    if (target.isPrivate) {
-      target.followRequests.addToSet(me._id);
+  if (target.isPrivate) {
+    if (!target.followRequests.includes(userId)) {
+      target.followRequests.push(userId);
       await target.save();
-      return res.json({ requested: true });
     }
-    me.following.addToSet(target._id);
-    target.followers.addToSet(me._id);
+    return res.json({ requested: true });
   }
 
-  await me.save();
-  await target.save();
+  await User.findByIdAndUpdate(targetId, {
+    $addToSet: { followers: userId },
+    $inc: { followersCount: 1 },
+  });
 
-  res.json({ following: !isFollowing });
+  await User.findByIdAndUpdate(userId, {
+    $addToSet: { following: targetId },
+    $inc: { followingCount: 1 },
+  });
+
+  res.json({ following: true });
 }
+
+// UNFOLLOW
+export async function unfollowUser(req, res) {
+  const targetId = req.params.id;
+  const userId = req.user.id;
+
+  await User.findByIdAndUpdate(targetId, {
+    $pull: { followers: userId },
+    $inc: { followersCount: -1 },
+  });
+
+  await User.findByIdAndUpdate(userId, {
+    $pull: { following: targetId },
+    $inc: { followingCount: -1 },
+  });
+
+  res.json({ following: false });
+}
+
+// ACCEPT FOLLOW REQUEST
+export async function acceptRequest(req, res) {
+  const requesterId = req.params.id;
+  const userId = req.user.id;
+
+  await User.findByIdAndUpdate(userId, {
+    $pull: { followRequests: requesterId },
+    $addToSet: { followers: requesterId },
+    $inc: { followersCount: 1 },
+  });
+
+  await User.findByIdAndUpdate(requesterId, {
+    $addToSet: { following: userId },
+    $inc: { followingCount: 1 },
+  });
+
+  res.json({ accepted: true });
+}
+
+// REJECT FOLLOW REQUEST
+export async function rejectRequest(req, res) {
+  const requesterId = req.params.id;
+  const userId = req.user.id;
+
+  await User.findByIdAndUpdate(userId, {
+    $pull: { followRequests: requesterId },
+  });
+
+  res.json({ rejected: true });
+      }
