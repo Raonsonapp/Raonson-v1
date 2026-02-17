2ddbd19306eff448c6cd2d2739fe2b9b3adddd55
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 20:52:56 +0500

    Update app_config.dart

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/widgets.dart';
 
 class AppConfig {
+  static late String appName;
   static late String apiBaseUrl;
   static late bool enableLogs;
 
   static Future<void> initialize({
+    required String appName,
     required String baseUrl,
     bool logs = true,
   }) async {
+    WidgetsFlutterBinding.ensureInitialized();
+    AppConfig.appName = appName;
     apiBaseUrl = baseUrl;
     enableLogs = logs;
-    WidgetsFlutterBinding.ensureInitialized();
   }
 }
