b51b12e5b6c693793dc92499dcf32e3019c5a15c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 12:13:44 +0500

    Update like.routes.js

--- backend/src/routes/like.routes.js
+++ backend/src/routes/like.routes.js
@@ -3,7 +3,7 @@ import {
   likeTarget,
   unlikeTarget,
 } from "../controllers/like.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
