db341d355b9a370e43c5e5d5a98a5b26a92c7eda
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 07:40:04 +0500

    Update follow.model.js

--- backend/src/models/follow.model.js
+++ backend/src/models/follow.model.js
@@ -2,15 +2,28 @@ import mongoose from "mongoose";
 
 const FollowSchema = new mongoose.Schema(
   {
-    from: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    to: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    follower: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    following: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     status: {
       type: String,
       enum: ["pending", "accepted"],
-      default: "pending",
+      default: "accepted",
     },
   },
   { timestamps: true }
+);
+
+FollowSchema.index(
+  { follower: 1, following: 1 },
+  { unique: true }
 );
 
 export const Follow = mongoose.model("Follow", FollowSchema);
