df589ef1d93dd0e16fdb615cd87b0305a3915f3c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 07:01:19 +0500

    Update like.controller.js

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -1,20 +1,16 @@
-import Like from "../models/like.model.js";
-import Post from "../models/post.model.js";
+import { Post } from "../models/post.model.js";
 
-// LIKE / UNLIKE (toggle)
-export const toggleLike = async (req, res) => {
-  const { postId } = req.params;
-  const userId = req.user.id;
+export async function toggleLike(req, res) {
+  const post = await Post.findById(req.params.postId);
 
-  const existing = await Like.findOne({ userId, postId });
+  const liked = post.likes.includes(req.user._id);
 
-  if (existing) {
-    await existing.deleteOne();
-    await Post.findByIdAndUpdate(postId, { $inc: { likesCount: -1 } });
-    return res.json({ liked: false });
-  } else {
-    await Like.create({ userId, postId });
-    await Post.findByIdAndUpdate(postId, { $inc: { likesCount: 1 } });
-    return res.json({ liked: true });
-  }
-};
+  await Post.findByIdAndUpdate(
+    req.params.postId,
+    liked
+      ? { $pull: { likes: req.user._id } }
+      : { $addToSet: { likes: req.user._id } }
+  );
+
+  res.json({ liked: !liked });
+}
