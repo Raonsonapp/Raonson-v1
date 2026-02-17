603eca65f88ba912306c600b3c47b0a46d4aab53
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 07:05:58 +0500

    Update auth.service.js

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,37 +1,16 @@
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
-import { jwtConfig } from "../config/jwt.config.js";
+import { JWT_SECRET, JWT_EXPIRES } from "../config/jwt.config.js";
 
-export async function registerUser({ username, email, password }) {
-  const exists = await User.findOne({ username });
-  if (exists) {
-    throw new Error("Username already taken");
-  }
-
-  const hash = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    username,
-    email,
-    password: hash,
-  });
-
-  return user;
+export function generateToken(user) {
+  return jwt.sign(
+    { id: user._id },
+    JWT_SECRET,
+    { expiresIn: JWT_EXPIRES }
+  );
 }
 
-export async function loginUser({ username, password }) {
-  const user = await User.findOne({ username }).select("+password");
-  if (!user) throw new Error("Invalid credentials");
-
-  const ok = await bcrypt.compare(password, user.password);
-  if (!ok) throw new Error("Invalid credentials");
-
-  const token = jwt.sign(
-    { id: user._id },
-    jwtConfig.secret,
-    { expiresIn: jwtConfig.expiresIn }
-  );
-
-  return { user, token };
-    }
+export async function getUserFromToken(token) {
+  const decoded = jwt.verify(token, JWT_SECRET);
+  return User.findById(decoded.id);
+}
