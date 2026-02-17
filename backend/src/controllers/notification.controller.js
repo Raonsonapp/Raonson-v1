96c7130edf2f524df6f67133b0abec3030f15ece
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 07:03:16 +0500

    Update notification.controller.js

--- backend/src/controllers/notification.controller.js
+++ backend/src/controllers/notification.controller.js
@@ -1,35 +1,16 @@
 import { Notification } from "../models/notification.model.js";
-import { emitToUser } from "../socket/index.js";
 
 export async function getNotifications(req, res) {
-  const items = await Notification.find({ to: req.user._id })
-    .populate("from", "username avatar")
+  const notifications = await Notification.find({ to: req.user._id })
     .sort({ createdAt: -1 });
 
-  res.json(items);
-}
-
-export async function createNotification({
-  to,
-  from,
-  type,
-  post,
-  story,
-}) {
-  const n = await Notification.create({
-    to,
-    from,
-    type,
-    post,
-    story,
-  });
-
-  emitToUser(to, "notification", n);
+  res.json(notifications);
 }
 
 export async function markSeen(req, res) {
   await Notification.findByIdAndUpdate(req.params.id, {
     seen: true,
   });
-  res.json({ ok: true });
+
+  res.json({ seen: true });
 }
