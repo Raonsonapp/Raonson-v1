2bc2a68728c13d8526e77681c1b3faa2d806c11f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 12:11:50 +0500

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,7 +5,7 @@ import {
   refreshToken,
   logout,
 } from "../controllers/auth.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
