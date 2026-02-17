2661803a1ffa859eea9cc27b38618284b45f635d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 19:51:30 +0500

    Delete lib/profile directory

--- lib/profile/profile_api.dart
+++
@@ -1,9 +0,0 @@
-import '../core/api.dart';
-import 'dart:convert';
-
-class ProfileApi {
-  static Future<Map<String, dynamic>> fetch(String username) async {
-    final res = await Api.get('/profile/$username');
-    return jsonDecode(res.body);
-  }
-}
