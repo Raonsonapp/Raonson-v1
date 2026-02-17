3b4099f63389420c5b4c0cc58374842352169769
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 06:57:40 +0500

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,20 +1,59 @@
-import { registerUser, loginUser } from "../services/auth.service.js";
-import { formatResponse } from "../core/responseFormatter.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { User } from "../models/user.model.js";
+import { jwtConfig } from "../config/jwt.config.js";
 
-export async function register(req, res, next) {
-  try {
-    const user = await registerUser(req.body);
-    res.json(formatResponse(true, "Registered", user));
-  } catch (e) {
-    next(e);
-  }
+function generateToken(user) {
+  return jwt.sign(
+    { id: user._id },
+    jwtConfig.secret,
+    { expiresIn: jwtConfig.expiresIn }
+  );
 }
 
-export async function login(req, res, next) {
-  try {
-    const { user, token } = await loginUser(req.body);
-    res.json(formatResponse(true, "Logged in", { user, token }));
-  } catch (e) {
-    next(e);
+export async function register(req, res) {
+  const { username, password } = req.body;
+
+  const exists = await User.findOne({ username });
+  if (exists) {
+    return res.status(400).json({ message: "Username already taken" });
   }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    username,
+    password: hash,
+  });
+
+  const token = generateToken(user);
+
+  res.json({ user, token });
 }
+
+export async function login(req, res) {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = generateToken(user);
+
+  res.json({ user, token });
+}
+
+export async function refreshToken(req, res) {
+  const token = generateToken(req.user);
+  res.json({ token });
+}
+
+export async function logout(req, res) {
+  res.json({ success: true });
+    }
