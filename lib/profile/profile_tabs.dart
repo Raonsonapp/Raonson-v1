744fd77e3eff345b5640c49e859a8269f3017b53
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 19:53:01 +0500

    Create profile_tabs.dart

---
+++ lib/profile/profile_tabs.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../models/post_model.dart';
+import '../models/reel_model.dart';
+import '../widgets/media_viewer.dart';
+
+class ProfileTabs extends StatelessWidget {
+  final List<PostModel> posts;
+  final List<ReelModel> reels;
+
+  const ProfileTabs({
+    super.key,
+    required this.posts,
+    required this.reels,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.grid_on)),
+              Tab(icon: Icon(Icons.video_collection)),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              children: [
+                _grid(posts.map((p) => p.mediaUrl).toList()),
+                _grid(reels.map((r) => r.videoUrl).toList()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _grid(List<String> urls) {
+    if (urls.isEmpty) {
+      return const Center(child: Text('No content'));
+    }
+
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 2,
+        crossAxisSpacing: 2,
+      ),
+      itemCount: urls.length,
+      itemBuilder: (_, i) => MediaViewer(url: urls[i]),
+    );
+  }
+}
