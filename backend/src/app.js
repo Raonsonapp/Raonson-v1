5ffc66c37e4511daa60b9d00cec6e96c58013e6a
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 16:04:47 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(rateLimitMiddleware);
+app.use(rateLimitMiddleware());
 
 // ================= HEALTH =================
 app.get("/", (req, res) => {
