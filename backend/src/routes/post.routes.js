0f8dc924dc1f5763ecc7c68dc5d86ac14c1a773d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 12:14:58 +0500

    Update post.routes.js

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -5,7 +5,7 @@ import {
   toggleLike,
   toggleSave,
 } from "../controllers/post.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
