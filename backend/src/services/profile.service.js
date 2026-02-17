79c99fecfeea476af48f23226280ba207f7a14cd
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 07:06:37 +0500

    Update profile.service.js

--- backend/src/services/profile.service.js
+++ backend/src/services/profile.service.js
@@ -1,17 +1,15 @@
-import { Follow } from "../models/follow.model.js";
 import { User } from "../models/user.model.js";
+import { Post } from "../models/post.model.js";
 
-export async function canViewProfile({ viewer, owner }) {
-  if (viewer.equals(owner)) return true;
+export async function getProfile(username) {
+  const user = await User.findOne({ username })
+    .select("username avatar verified isPrivate followersCount followingCount postsCount");
 
-  const user = await User.findById(owner);
-  if (!user.isPrivate) return true;
+  if (!user) return null;
 
-  const follow = await Follow.findOne({
-    from: viewer,
-    to: owner,
-    status: "accepted",
-  });
+  const posts = await Post.find({ user: user._id })
+    .sort({ createdAt: -1 })
+    .limit(12);
 
-  return !!follow;
+  return { user, posts };
 }
