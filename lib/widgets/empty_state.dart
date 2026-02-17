29fdde8dafbebda67a9233a90e49bbab1201b6d0
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 20:15:01 +0500

    Create empty_state.dart

---
+++ lib/widgets/empty_state.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String? subtitle;
+
+  const EmptyState({
+    super.key,
+    required this.icon,
+    required this.title,
+    this.subtitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 64, color: Colors.grey),
+            const SizedBox(height: 16),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.titleMedium,
+              textAlign: TextAlign.center,
+            ),
+            if (subtitle != null) ...[
+              const SizedBox(height: 8),
+              Text(
+                subtitle!,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium
+                    ?.copyWith(color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            ]
+          ],
+        ),
+      ),
+    );
+  }
+}
