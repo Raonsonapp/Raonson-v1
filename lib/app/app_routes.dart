ec98f3c00b1bec5a6b1238563f18fd25e0fd938c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 22:53:48 +0500

    Update app_routes.dart

--- lib/app/app_routes.dart
+++ lib/app/app_routes.dart
@@ -2,9 +2,15 @@ abstract class AppRoutes {
   static const String splash = '/';
   static const String login = '/login';
   static const String register = '/register';
+
+  // Bottom tabs
   static const String home = '/home';
-  static const String profile = '/profile';
-  static const String create = '/create';
+  static const String reels = '/reels';
   static const String chat = '/chat';
+  static const String search = '/search';
+  static const String profile = '/profile';
+
+  // Home actions
+  static const String create = '/create';
   static const String notifications = '/notifications';
 }
