3f686339caadd15f8db92e23c189afc5f4e39025
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-17 18:15:30 +0500

    Create app_controller.dart

---
+++ lib/app/app_controller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'app_state.dart';
+import 'app_routes.dart';
+
+class AppController {
+  final AppState appState;
+
+  AppController(this.appState);
+
+  String get initialRoute {
+    if (!appState.isInitialized) return AppRoutes.splash;
+    return appState.isAuthenticated
+        ? AppRoutes.home
+        : AppRoutes.login;
+  }
+
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.login:
+        return _page(const Placeholder());
+      case AppRoutes.register:
+        return _page(const Placeholder());
+      case AppRoutes.home:
+        return _page(const Placeholder());
+      case AppRoutes.profile:
+        return _page(const Placeholder());
+      case AppRoutes.create:
+        return _page(const Placeholder());
+      case AppRoutes.chat:
+        return _page(const Placeholder());
+      case AppRoutes.notifications:
+        return _page(const Placeholder());
+      default:
+        return _page(const Placeholder());
+    }
+  }
+
+  MaterialPageRoute _page(Widget child) {
+    return MaterialPageRoute(builder: (_) => child);
+  }
+}
