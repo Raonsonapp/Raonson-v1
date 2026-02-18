fffb8fc6deb27dbc218c47af2311b4ab65ee6e3f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 16:20:10 +0500

    Update profile_repository.dart

--- lib/profile/profile_repository.dart
+++ lib/profile/profile_repository.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+
 import '../core/api/api_client.dart';
 import '../core/api/api_endpoints.dart';
 import '../models/user_model.dart';
+import '../models/post_model.dart';
 import '../models/reel_model.dart';
 
 class ProfileRepository {
@@ -19,17 +21,31 @@ class ProfileRepository {
   Future<void> updateProfile({
     required String username,
     String? bio,
+    bool? isPrivate,
   }) async {
     await _api.post(
       ApiEndpoints.updateProfile,
       body: {
         'username': username,
         'bio': bio,
+        'isPrivate': isPrivate,
       },
     );
   }
 
   // ================= FOLLOW =================
+
+  Future<List<UserModel>> getFollowers(String userId) async {
+    final res = await _api.get('/users/$userId/followers');
+    final List list = jsonDecode(res.body);
+    return list.map((e) => UserModel.fromJson(e)).toList();
+  }
+
+  Future<List<UserModel>> getFollowing(String userId) async {
+    final res = await _api.get('/users/$userId/following');
+    final List list = jsonDecode(res.body);
+    return list.map((e) => UserModel.fromJson(e)).toList();
+  }
 
   Future<void> follow(String userId) {
     return _api.post('/follow/$userId');
@@ -37,6 +53,14 @@ class ProfileRepository {
 
   Future<void> unfollow(String userId) {
     return _api.post('/unfollow/$userId');
+  }
+
+  // ================= POSTS =================
+
+  Future<List<PostModel>> getUserPosts(String userId) async {
+    final res = await _api.get('/users/$userId/posts');
+    final List list = jsonDecode(res.body);
+    return list.map((e) => PostModel.fromJson(e)).toList();
   }
 
   // ================= REELS =================
