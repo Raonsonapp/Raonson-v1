f261bfd49e7ce7d1178056c9da32f95125827079
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 13:53:36 +0500

    Update comments_screen.dart

--- lib/feed/comments/comments_screen.dart
+++ lib/feed/comments/comments_screen.dart
@@ -1,43 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../../models/post_model.dart';
+import '../../models/comment_model.dart';
 import 'comment_item.dart';
-import 'comment_input.dart';
 
 class CommentsScreen extends StatelessWidget {
   final PostModel post;
+  final List<CommentModel> comments;
 
   const CommentsScreen({
     super.key,
     required this.post,
+    required this.comments,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Comments'),
-        elevation: 1,
-      ),
-      body: Column(
-        children: [
-          // ================= COMMENTS LIST =================
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              itemCount: post.comments.length,
-              itemBuilder: (context, index) {
-                final comment = post.comments[index];
-                return CommentItem(comment: comment);
-              },
-            ),
-          ),
-
-          // ================= INPUT =================
-          CommentInput(
-            postId: post.id,
-          ),
-        ],
+      appBar: AppBar(title: const Text('Comments')),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: comments.length,
+        itemBuilder: (_, i) {
+          return CommentItem(comment: comments[i]);
+        },
       ),
     );
   }
