d69b93dac13ad427fc3ba5dd327242baf2300a7e
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 15:59:02 +0500

    Update reel_model.dart

--- lib/models/reel_model.dart
+++ lib/models/reel_model.dart
@@ -1,38 +1,19 @@
-import 'user_model.dart';
-
 class ReelModel {
   final String id;
-  final UserModel user;
   final String videoUrl;
-  final int likes;
-  final int views;
-  final bool liked;
-  final bool saved;
+  final String caption;
 
   const ReelModel({
     required this.id,
-    required this.user,
     required this.videoUrl,
-    required this.likes,
-    required this.views,
-    required this.liked,
-    required this.saved,
+    required this.caption,
   });
-
-  // ---------- COMPAT GETTERS ----------
-  int get likesCount => likes;
-  bool get isLiked => liked;
-  int get commentsCount => 0;
 
   factory ReelModel.fromJson(Map<String, dynamic> json) {
     return ReelModel(
       id: json['_id'],
-      user: UserModel.fromJson(json['user']),
       videoUrl: json['videoUrl'] ?? '',
-      likes: json['likes'] ?? 0,
-      views: json['views'] ?? 0,
-      liked: json['liked'] ?? false,
-      saved: json['saved'] ?? false,
+      caption: json['caption'] ?? '',
     );
   }
 }
