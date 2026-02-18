4047cb58777d5a18b9cc822aec7e5af61083bc66
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 12:44:02 +0500

    Update route_guards.dart

--- lib/navigation/route_guards.dart
+++ lib/navigation/route_guards.dart
@@ -1,38 +1,19 @@
-import 'package:flutter/material.dart';
+abstract class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
 
-import '../app/app_state.dart';
-import '../app/app_routes.dart';
+  static const String otpVerify = '/otp';
+  static const String forgotPassword = '/forgot-password';
+  static const String resetPassword = '/reset-password';
 
-class RouteGuards {
-  static Route<dynamic>? onGenerateRouteGuarded(
-    RouteSettings settings,
-    AppState appState,
-  ) {
-    final routeName = settings.name ?? '';
+  // Bottom tabs
+  static const String home = '/home';
+  static const String reels = '/reels';
+  static const String chat = '/chat';
+  static const String search = '/search';
+  static const String profile = '/profile';
 
-    if (_isPublicRoute(routeName)) {
-      return null;
-    }
-
-    if (!appState.isAuthenticated) {
-      return _redirectToLogin();
-    }
-
-    return null;
-  }
-
-  static bool _isPublicRoute(String route) {
-    return route == AppRoutes.login ||
-        route == AppRoutes.register ||
-        route == AppRoutes.otpVerify ||
-        route == AppRoutes.forgotPassword ||
-        route == AppRoutes.resetPassword;
-  }
-
-  static Route _redirectToLogin() {
-    return MaterialPageRoute(
-      settings: const RouteSettings(name: AppRoutes.login),
-      builder: (_) => const SizedBox.shrink(),
-    );
-  }
+  // Actions
+  static const String notifications = '/notifications';
 }
