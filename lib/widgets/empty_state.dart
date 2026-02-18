ddc991895cc878b5df50443b68dd97433c0aa543
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 13:20:45 +0500

    Update empty_state.dart

--- lib/widgets/empty_state.dart
+++ lib/widgets/empty_state.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class EmptyState extends StatelessWidget {
-  final IconData icon;
+  final IconData? icon;
   final String title;
   final String? subtitle;
 
   const EmptyState({
     super.key,
-    required this.icon,
+    this.icon,
     required this.title,
     this.subtitle,
   });
@@ -20,7 +20,11 @@ class EmptyState extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(icon, size: 64, color: Colors.grey),
+            Icon(
+              icon ?? Icons.inbox_outlined,
+              size: 64,
+              color: Colors.grey,
+            ),
             const SizedBox(height: 16),
             Text(
               title,
@@ -37,7 +41,7 @@ class EmptyState extends StatelessWidget {
                     ?.copyWith(color: Colors.grey),
                 textAlign: TextAlign.center,
               ),
-            ]
+            ],
           ],
         ),
       ),
