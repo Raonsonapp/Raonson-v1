23d1c10384cee072cfadfac8d192cec82ad6ac4d
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 14:07:39 +0500

    Update message_bubble.dart

--- lib/chat/room/message_bubble.dart
+++ lib/chat/room/message_bubble.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../models/message_model.dart';
 import '../../theme/colors.dart';
 
@@ -13,33 +12,23 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isMe = message.isMine;
+    final isMe = false; // TODO: compare with current user id
 
     return Align(
       alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 4),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 14,
-          vertical: 10,
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
         constraints: const BoxConstraints(maxWidth: 280),
         decoration: BoxDecoration(
           color: isMe ? AppColors.primary : AppColors.surface,
-          borderRadius: BorderRadius.only(
-            topLeft: const Radius.circular(16),
-            topRight: const Radius.circular(16),
-            bottomLeft:
-                isMe ? const Radius.circular(16) : Radius.zero,
-            bottomRight:
-                isMe ? Radius.zero : const Radius.circular(16),
-          ),
+          borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              message.text,
+              message.lastMessage,
               style: TextStyle(
                 color: isMe ? Colors.white : Colors.black87,
                 fontSize: 15,
@@ -50,9 +39,7 @@ class MessageBubble extends StatelessWidget {
               message.timeLabel,
               style: TextStyle(
                 fontSize: 11,
-                color: isMe
-                    ? Colors.white70
-                    : Colors.grey.shade600,
+                color: isMe ? Colors.white70 : Colors.grey,
               ),
             ),
           ],
