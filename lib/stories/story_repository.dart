23792f77396576aaaac5bc703f4b28597cdb7461
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-18 15:19:18 +0500

    Update story_repository.dart

--- lib/stories/story_repository.dart
+++ lib/stories/story_repository.dart
@@ -5,20 +5,18 @@ import '../core/api/api_endpoints.dart';
 import '../models/story_model.dart';
 
 class StoryRepository {
-  /// GET STORIES
-  Future<List<StoryModel>> fetchStories() async {
-    final res = await ApiClient.get(
-      ApiEndpoints.stories,
-    );
+  final ApiClient _api;
 
-    final List list = jsonDecode(res.body);
-    return list.map((e) => StoryModel.fromJson(e)).toList();
+  StoryRepository(this._api);
+
+  Future<List<StoryModel>> fetchStories() async {
+    final res = await _api.get(ApiEndpoints.stories);
+    return (jsonDecode(res.data) as List)
+        .map((e) => StoryModel.fromJson(e))
+        .toList();
   }
 
-  /// MARK STORY AS VIEWED
   Future<void> markStoryViewed(String storyId) async {
-    await ApiClient.post(
-      '${ApiEndpoints.stories}/$storyId/view',
-    );
+    await _api.post('${ApiEndpoints.stories}/$storyId/view');
   }
 }
