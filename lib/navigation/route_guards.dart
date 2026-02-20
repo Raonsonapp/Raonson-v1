a5d731b49dfd1995c993092dec8173b5ee3e16f9
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-20 16:01:37 +0500

    Add files via upload

---
+++ lib/navigation/route_guards.dart
@@ -0,0 +1,17 @@
+abstract class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+
+  static const String otpVerify = '/otp';
+  static const String forgotPassword = '/forgot-password';
+  static const String resetPassword = '/reset-password';
+
+  static const String home = '/home';
+  static const String reels = '/reels';
+  static const String chat = '/chat';
+  static const String search = '/search';
+  static const String profile = '/profile';
+
+  static const String notifications = '/notifications';
+}
