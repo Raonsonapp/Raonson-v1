496d003861df728eab14509f226435d887e7f677
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-20 16:42:49 +0500

    Add files via upload

---
+++ backend/src/data/reels.store.js
@@ -0,0 +1,11 @@
+export const reels = [
+  {
+    id: "1",
+    userId: "u1",
+    videoUrl: "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+    caption: "Sunset vibes 🌅",
+    likes: 0,
+    views: 0,
+    comments: 0,
+  },
+];
---
+++ backend/src/data/users.store.js
@@ -0,0 +1,14 @@
+export const users = [
+  {
+    id: "u1",
+    username: "olivia_martin",
+    avatar: "https://i.pravatar.cc/150?img=32",
+    verified: true,
+  },
+  {
+    id: "u2",
+    username: "alex_dev",
+    avatar: "https://i.pravatar.cc/150?img=12",
+    verified: false,
+  },
+];
