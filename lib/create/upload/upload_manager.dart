37d0227e9d1da0037c09dab58051c350119d19aa
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-20 15:46:43 +0500

    Create upload_manager.dart

---
+++ lib/create/upload/upload_manager.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+import '../../app/app_config.dart';
+import '../../core/storage/token_storage.dart';
+
+class UploadManager {
+  /// =========================
+  /// UPLOAD STORY
+  /// =========================
+  Future<void> uploadStory({
+    required File file,
+    String caption = '',
+    void Function(double progress)? onProgress,
+  }) async {
+    final token = await TokenStorage.getAccessToken();
+
+    final uri = Uri.parse('${AppConfig.apibaseUrl}/stories');
+
+    final request = http.MultipartRequest('POST', uri);
+
+    request.headers['Authorization'] = 'Bearer $token';
+    request.fields['caption'] = caption;
+
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'media',
+        file.path,
+      ),
+    );
+
+    final streamed = await request.send();
+
+    if (streamed.statusCode >= 400) {
+      throw Exception('Story upload failed');
+    }
+  }
+
+  /// =========================
+  /// UPLOAD POST
+  /// =========================
+  Future<void> uploadPost({
+    required List<File> media,
+    required String caption,
+    void Function(double progress)? onProgress,
+  }) async {
+    final token = await TokenStorage.getAccessToken();
+
+    final uri = Uri.parse('${AppConfig.apibaseUrl}/posts');
+
+    final request = http.MultipartRequest('POST', uri);
+
+    request.headers['Authorization'] = 'Bearer $token';
+    request.fields['caption'] = caption;
+
+    for (final file in media) {
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'media',
+          file.path,
+        ),
+      );
+    }
+
+    final streamed = await request.send();
+
+    if (streamed.statusCode >= 400) {
+      throw Exception('Post upload failed');
+    }
+  }
+}
