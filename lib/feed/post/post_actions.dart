59bb98f15d62b254d5b5ebcb136735ddea2c6e10
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-22 11:10:10 +0500

    Update post_actions.dart

--- lib/feed/post/post_actions.dart
+++ lib/feed/post/post_actions.dart
@@ -24,17 +24,17 @@ class _PostActionsState extends State<PostActions> {
   }
 
   Future<void> _toggleLike() async {
+    final newLiked = !_liked;
     setState(() {
-      _liked = !_liked;
-      _likeCount += _liked ? 1 : -1;
+      _liked = newLiked;
+      _likeCount += newLiked ? 1 : -1;
     });
     try {
       await ApiClient.instance.post('/posts/${widget.post.id}/like');
     } catch (_) {
-      // revert on error
       setState(() {
-        _liked = !_liked;
-        _likeCount += _liked ? 1 : -1;
+        _liked = !newLiked;
+        _likeCount += newLiked ? -1 : 1;
       });
     }
   }
@@ -48,52 +48,37 @@ class _PostActionsState extends State<PostActions> {
     }
   }
 
+  int get likeCount => _likeCount;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8, 2, 8, 0),
       child: Row(
         children: [
-          // Like
-          _Btn(
-            onTap: _toggleLike,
-            child: AnimatedSwitcher(
-              duration: const Duration(milliseconds: 180),
-              child: _liked
-                  ? const Icon(Icons.favorite,
-                      key: ValueKey(true), color: Colors.red, size: 26)
-                  : const Icon(Icons.favorite_border,
-                      key: ValueKey(false), color: Colors.white, size: 26),
-            ),
-          ),
-          // Comment
-          _Btn(
-            onTap: () {},
-            child: const Icon(Icons.mode_comment_outlined,
-                color: Colors.white, size: 24),
-          ),
-          // Share
-          _Btn(
-            onTap: () {},
-            child: Transform.rotate(
-              angle: -0.4,
-              child: const Icon(Icons.send_outlined,
-                  color: Colors.white, size: 23),
-            ),
-          ),
+          _Btn(onTap: _toggleLike, child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 180),
+            child: _liked
+                ? const Icon(Icons.favorite,
+                    key: ValueKey(true), color: Colors.red, size: 26)
+                : const Icon(Icons.favorite_border,
+                    key: ValueKey(false), color: Colors.white, size: 26),
+          )),
+          _Btn(onTap: () {}, child: const Icon(
+              Icons.mode_comment_outlined, color: Colors.white, size: 24)),
+          _Btn(onTap: () {}, child: Transform.rotate(
+            angle: -0.4,
+            child: const Icon(Icons.send_outlined, color: Colors.white, size: 23),
+          )),
           const Spacer(),
-          // Bookmark
-          _Btn(
-            onTap: _toggleSave,
-            child: AnimatedSwitcher(
-              duration: const Duration(milliseconds: 180),
-              child: _saved
-                  ? const Icon(Icons.bookmark,
-                      key: ValueKey(true), color: Colors.white, size: 26)
-                  : const Icon(Icons.bookmark_border,
-                      key: ValueKey(false), color: Colors.white, size: 26),
-            ),
-          ),
+          _Btn(onTap: _toggleSave, child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 180),
+            child: _saved
+                ? const Icon(Icons.bookmark,
+                    key: ValueKey(true), color: Colors.white, size: 26)
+                : const Icon(Icons.bookmark_border,
+                    key: ValueKey(false), color: Colors.white, size: 26),
+          )),
         ],
       ),
     );
@@ -104,10 +89,8 @@ class _Btn extends StatelessWidget {
   final VoidCallback onTap;
   final Widget child;
   const _Btn({required this.onTap, required this.child});
-
   @override
   Widget build(BuildContext context) => GestureDetector(
-        onTap: onTap,
-        child: Padding(padding: const EdgeInsets.all(6), child: child),
-      );
+      onTap: onTap,
+      child: Padding(padding: const EdgeInsets.all(6), child: child));
 }
