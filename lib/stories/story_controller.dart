535f9b34a9c32a3292844346d842af430ef9315f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-24 07:18:33 +0500

    Update story_controller.dart

--- lib/stories/story_controller.dart
+++ lib/stories/story_controller.dart
@@ -9,17 +9,24 @@ class StoryController extends ChangeNotifier {
   StoryController(this._repository);
 
   List<StoryModel> _stories = [];
+  List<StoryModel> _myStories = [];
   bool _loading = false;
 
   List<StoryModel> get stories => _stories;
+  List<StoryModel> get myStories => _myStories;
+  bool get hasMyStory => _myStories.isNotEmpty;
   bool get isLoading => _loading;
 
   Future<void> loadStories() async {
     _loading = true;
     notifyListeners();
-
     try {
-      _stories = await _repository.fetchStories();
+      final all = await _repository.fetchStories();
+      final my = await _repository.fetchMyStories();
+      _myStories = my;
+      // Filter out own stories from main list
+      final myIds = my.map((s) => s.id).toSet();
+      _stories = all.where((s) => !myIds.contains(s.id)).toList();
     } finally {
       _loading = false;
       notifyListeners();
