01c8eafef833e14aac37747efd5ce8956bcf6083
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-26 13:11:35 +0500

    Update story.routes.js

--- backend/src/routes/story.routes.js
+++ backend/src/routes/story.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getStories,
   getMyStories,
+  getViewers,
   createStory,
   viewStory,
   likeStory,
@@ -13,6 +14,7 @@ const router = express.Router();
 
 router.get("/", authMiddleware, getStories);
 router.get("/my", authMiddleware, getMyStories);
+router.get("/:id/viewers", authMiddleware, getViewers);
 router.post("/", authMiddleware, createStory);
 router.delete("/:id", authMiddleware, deleteStory);
 router.post("/:id/view", authMiddleware, viewStory);
