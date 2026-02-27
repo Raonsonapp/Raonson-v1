5b54a605c856109421f0e6794ed069491804acff
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-27 11:24:40 +0500

    Update reel.model.js

--- backend/src/models/reel.model.js
+++ backend/src/models/reel.model.js
@@ -14,6 +14,11 @@ const ReelSchema = new mongoose.Schema(
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     saves: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     views: { type: Number, default: 0 },
+    comments: [{
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      text: { type: String, required: true },
+      createdAt: { type: Date, default: Date.now },
+    }],
   },
   { timestamps: true }
 );
