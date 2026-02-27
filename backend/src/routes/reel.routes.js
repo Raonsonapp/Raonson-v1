f385eb38bc1a13b3c0898f3e1c46a3226bd96125
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-27 11:26:20 +0500

    Update reel.routes.js

--- backend/src/routes/reel.routes.js
+++ backend/src/routes/reel.routes.js
@@ -6,6 +6,8 @@ import {
   toggleLike,
   toggleSave,
   deleteReel,
+  getReelComments,
+  addReelComment,
 } from "../controllers/reel.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
@@ -17,5 +19,7 @@ router.delete("/:id", authMiddleware, deleteReel);
 router.post("/:id/view", authMiddleware, addView);
 router.post("/:id/like", authMiddleware, toggleLike);
 router.post("/:id/save", authMiddleware, toggleSave);
+router.get("/:id/comments", authMiddleware, getReelComments);
+router.post("/:id/comments", authMiddleware, addReelComment);
 
 export default router;
