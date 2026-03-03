9f728e050d2ad030d1f84583a09482742a8cd9a6
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-03 22:37:02 +0500

    Update socket_service.dart

--- lib/core/services/socket_service.dart
+++ lib/core/services/socket_service.dart
@@ -1,18 +1,15 @@
 import 'package:socket_io_client/socket_io_client.dart' as IO;
-import '../app/app_config.dart';
+import '../../app/app_config.dart';
 
 class SocketService {
   SocketService._();
   static final SocketService instance = SocketService._();
 
   IO.Socket? _socket;
-  IO.Socket? get socket => _socket;
-
   bool get isConnected => _socket?.connected ?? false;
 
   void connect(String token) {
     if (isConnected) return;
-
     _socket = IO.io(
       AppConfig.apiBaseUrl,
       IO.OptionBuilder()
@@ -21,41 +18,34 @@ class SocketService {
           .setAuth({'token': token})
           .build(),
     );
-
     _socket!.connect();
-    _socket!.on('connect', (_) => print('[Socket] connected'));
-    _socket!.on('disconnect', (_) => print('[Socket] disconnected'));
-    _socket!.on('connect_error', (e) => print('[Socket] error: $e'));
   }
 
-  void joinChat(String chatId) {
-    _socket?.emit('chat:join', {'conversationId': chatId});
-  }
+  void joinChat(String chatId) =>
+      _socket?.emit('chat:join', {'conversationId': chatId});
 
   void leaveChat(String chatId) {
     _socket?.off('chat:new');
+    _socket?.off('chat:typing');
   }
 
-  void sendTyping(String chatId, String userId) {
-    _socket?.emit('chat:typing', {'conversationId': chatId, 'userId': userId});
-  }
+  void sendTyping(String chatId, String userId) =>
+      _socket?.emit('chat:typing', {'conversationId': chatId, 'userId': userId});
 
-  void onNewMessage(Function(Map<String, dynamic>) callback) {
+  void onNewMessage(void Function(Map<String, dynamic>) cb) {
     _socket?.on('chat:new', (data) {
-      if (data is Map<String, dynamic>) callback(data);
+      if (data is Map<String, dynamic>) cb(data);
     });
   }
 
-  void onTyping(Function(String) callback) {
+  void onTyping(void Function(String) cb) {
     _socket?.on('chat:typing', (data) {
-      if (data is Map && data['userId'] != null) {
-        callback(data['userId'] as String);
-      }
+      if (data is Map && data['userId'] != null) cb(data['userId'] as String);
     });
   }
 
-  void offTyping() => _socket?.off('chat:typing');
   void offNewMessage() => _socket?.off('chat:new');
+  void offTyping() => _socket?.off('chat:typing');
 
   void disconnect() {
     _socket?.disconnect();
