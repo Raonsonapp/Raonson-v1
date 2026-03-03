2178338cd7ded30853713512d5a110fa879a2027
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-03 22:15:36 +0500

    Update message_model.dart

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,33 +1,64 @@
-import 'user_model.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import '../app/app_config.dart';
 
-class MessageModel {
-  final String id;
-  final UserModel peer;
-  final String text;
-  final DateTime createdAt;
-  final bool isMine;
+class SocketService {
+  SocketService._();
+  static final SocketService instance = SocketService._();
 
-  const MessageModel({
-    required this.id,
-    required this.peer,
-    required this.text,
-    required this.createdAt,
-    required this.isMine,
-  });
+  IO.Socket? _socket;
+  IO.Socket? get socket => _socket;
 
-  // ---------- COMPAT ----------
-  String get lastMessage => text;
+  bool get isConnected => _socket?.connected ?? false;
 
-  String get timeLabel =>
-      '${createdAt.hour.toString().padLeft(2, '0')}:${createdAt.minute.toString().padLeft(2, '0')}';
+  void connect(String token) {
+    if (isConnected) return;
 
-  factory MessageModel.fromJson(Map<String, dynamic> json) {
-    return MessageModel(
-      id: json['_id'],
-      peer: UserModel.fromJson(json['peer']),
-      text: json['text'] ?? json['message'] ?? '',
-      createdAt: DateTime.parse(json['createdAt']),
-      isMine: json['isMine'] ?? false,
+    _socket = IO.io(
+      AppConfig.apiBaseUrl,
+      IO.OptionBuilder()
+          .setTransports(['websocket'])
+          .disableAutoConnect()
+          .setAuth({'token': token})
+          .build(),
     );
+
+    _socket!.connect();
+    _socket!.on('connect', (_) => print('[Socket] connected'));
+    _socket!.on('disconnect', (_) => print('[Socket] disconnected'));
+    _socket!.on('connect_error', (e) => print('[Socket] error: $e'));
+  }
+
+  void joinChat(String chatId) {
+    _socket?.emit('chat:join', {'conversationId': chatId});
+  }
+
+  void leaveChat(String chatId) {
+    _socket?.off('chat:new');
+  }
+
+  void sendTyping(String chatId, String userId) {
+    _socket?.emit('chat:typing', {'conversationId': chatId, 'userId': userId});
+  }
+
+  void onNewMessage(Function(Map<String, dynamic>) callback) {
+    _socket?.on('chat:new', (data) {
+      if (data is Map<String, dynamic>) callback(data);
+    });
+  }
+
+  void onTyping(Function(String) callback) {
+    _socket?.on('chat:typing', (data) {
+      if (data is Map && data['userId'] != null) {
+        callback(data['userId'] as String);
+      }
+    });
+  }
+
+  void offTyping() => _socket?.off('chat:typing');
+  void offNewMessage() => _socket?.off('chat:new');
+
+  void disconnect() {
+    _socket?.disconnect();
+    _socket = null;
   }
 }
