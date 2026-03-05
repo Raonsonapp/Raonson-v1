a3935b55e0a9e4a6e7a426ca5edc6bcb582f0a69
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-05 19:16:09 +0500

    Update chat_list_controller.dart

--- lib/chat/inbox/chat_list_controller.dart
+++ lib/chat/inbox/chat_list_controller.dart
@@ -11,16 +11,23 @@ class ChatListController extends ChangeNotifier {
   bool _loading = false;
   bool get isLoading => _loading;
 
+  String? _error;
+  String? get error => _error;
+
   List<MessageModel> _chats = [];
   List<MessageModel> get chats => List.unmodifiable(_chats);
 
   Future<void> loadChats() async {
     _loading = true;
+    _error = null;
     notifyListeners();
     try {
       _chats = await _repository.getInboxChats();
-    } catch (_) {
+      debugPrint('[Inbox] loaded ${_chats.length} chats');
+    } catch (e) {
+      debugPrint('[Inbox] ERROR: $e');
       _chats = [];
+      _error = e.toString();
     } finally {
       _loading = false;
       notifyListeners();
