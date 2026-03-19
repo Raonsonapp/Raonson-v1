73b1081764c44559cb09f967ee86dcd3f03c249b
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-19 20:23:49 +0500

    Update main.go

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ type User struct {
 	Password string `json:"password"`
 }
 
+var fakeUser User
+
 func main() {
 
 	port := os.Getenv("PORT")
@@ -27,6 +29,7 @@ func main() {
 	})
 
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	fmt.Println("Server running on port", port)
 	http.ListenAndServe(":"+port, nil)
@@ -40,21 +43,37 @@ func register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var user User
+	json.NewDecoder(r.Body).Decode(&user)
 
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid JSON", 400)
-		return
-	}
-
-	// simple validation
 	if user.Email == "" || user.Password == "" {
 		http.Error(w, "Missing fields", 400)
 		return
 	}
 
+	// save in memory (temporary)
+	fakeUser = user
+
 	json.NewEncoder(w).Encode(map[string]string{
-		"message":  "User registered ✅",
-		"username": user.Username,
+		"message": "Registered ✅",
 	})
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "POST only", 405)
+		return
+	}
+
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == fakeUser.Email && user.Password == fakeUser.Password {
+		json.NewEncoder(w).Encode(map[string]string{
+			"token": "raonson-token-123",
+		})
+		return
+	}
+
+	http.Error(w, "Invalid login", 401)
 }
