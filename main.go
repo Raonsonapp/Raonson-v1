6a85a01b4da462e7339bba3f54d6e4193cdf2438
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-19 20:14:06 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
-	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-
-	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
-
 func main() {
-	var err error
-
-	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Database connected ✅")
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -32,7 +15,15 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Backend + DB OK ✅")
+		json.NewEncoder(w).Encode(map[string]string{
+			"status": "ok",
+		})
+	})
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Register working ✅",
+		})
 	})
 
 	fmt.Println("Server running on port", port)
