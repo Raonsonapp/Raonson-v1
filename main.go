dd443783a96fe11ea1249dcb501682208c2bb737
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-19 20:16:54 +0500

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func main() {
 
 	port := os.Getenv("PORT")
@@ -20,12 +26,35 @@ func main() {
 		})
 	})
 
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(map[string]string{
-			"message": "Register working ✅",
-		})
-	})
+	http.HandleFunc("/register", register)
 
 	fmt.Println("Server running on port", port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "POST only", 405)
+		return
+	}
+
+	var user User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid JSON", 400)
+		return
+	}
+
+	// simple validation
+	if user.Email == "" || user.Password == "" {
+		http.Error(w, "Missing fields", 400)
+		return
+	}
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"message":  "User registered ✅",
+		"username": user.Username,
+	})
 }
