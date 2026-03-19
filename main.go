6a5f87bd7885304de0eeae1a5030a9b442eaa507
Raonson <ehsonmahmadmurodov@gmail.com>
2026-03-19 20:11:47 +0500

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
+	var err error
+
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database connected ✅")
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Backend working without DB ✅")
+		fmt.Fprintln(w, "Backend + DB OK ✅")
 	})
 
 	fmt.Println("Server running on port", port)
