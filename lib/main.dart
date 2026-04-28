2f606959bffd4cf0fa8251577bcde8d889b85cbf
Raonson <ehsonmahmadmurodov@gmail.com>
2026-04-28 12:07:04 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 import 'app/app_config.dart';
+import 'core/services/user_session.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +14,10 @@ Future<void> main() async {
     appName: 'Raonson',
     enableLogs: true,
   );
+
+  // Аватари кэшшударо аз SharedPreferences бор кун
+  // то интернет набошад ҳам аватар нишон дода шавад
+  await UserSession.loadCachedData();
 
   runApp(const RaonsonApp());
 }
