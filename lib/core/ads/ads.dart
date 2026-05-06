b00153ea1186a655748c48b7c5401ad16565fd49
Raonson <ehsonmahmadmurodov@gmail.com>
2026-05-07 01:59:28 +0500

    Create ads.dart

---
+++ lib/core/ads/ads.dart
@@ -0,0 +1,8 @@
+// lib/core/ads/ads.dart
+// Barrel export for all ad-related modules
+
+export 'ads_manager.dart';
+export 'feed_ad_card.dart';
+export 'ad_banner_widget.dart';
+export 'rewarded_ad_flow.dart';
+export 'yandex_ads.dart';
